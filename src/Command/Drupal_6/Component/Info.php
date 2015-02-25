efa93fa764659ec6d4fc84146a7cedcee5b911e0
Chi <Chi-teck@users.noreply.github.com>
2015-02-25 13:43:02 +0000

    Added d6 info file.

---
+++ src/Command/Drupal_6/Component/Info.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DrupalCodeGenerator\Command\Drupal_6\Component;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use DrupalCodeGenerator\Command\BaseGenerator;
+
+/**
+ * Class Info
+ * @package DrupalCodeGenerator\Command\Drupal_7\Component
+ */
+class Info extends BaseGenerator {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static  $name = 'generate:d6:component:info-file';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $description = 'Generate Drupal 6 .info file';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function interact(InputInterface $input, OutputInterface $output) {
+
+    $questions = [
+      'name' => ['Module name', [$this, 'defaultName']],
+      'machine_name' => ['Module machine name', [$this, 'defaultMachineName']],
+      'description' => ['Module description', 'TODO: Write description for the module'],
+      'package' => ['Package', 'custom'],
+      'version' => ['Version', '6.x-1.0-dev'],
+    ];
+
+    $vars = $this->collectVars($input, $output, $questions);
+
+    $prefix = $vars['machine_name'];
+    $this->files[$prefix . '.info'] = $this->render('d6/info.twig', $vars);
+
+  }
+
+}
--- src/Command/Generate.php
+++ src/Command/Generate.php
@@ -69,6 +69,9 @@ class Generate extends Command {
         'Module' => NULL,
         'Theme' => NULL,
         'Installation profile' => NULL,
+        'Component' => [
+          'Info file' => NULL,
+        ],
       ],
       'Other' => [
         'Drush command' => NULL,
