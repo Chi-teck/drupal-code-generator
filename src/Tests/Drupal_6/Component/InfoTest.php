558e50d75e3e1190cf8223d3152e039145d05a52
Chi <Chi-teck@users.noreply.github.com>
2015-02-25 13:46:21 +0000

    Added test for D6 info file.

---
+++ src/Tests/Drupal_6/Component/InfoTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DrupalCodeGenerator\Tests\Drupal_6\Component;
+
+use DrupalCodeGenerator\Tests\GeneratorTestCase;
+use DrupalCodeGenerator\Command\Drupal_6\Component\Info;
+
+class InfoTest extends GeneratorTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp () {
+    $this->command = new Info();
+    $this->commandName = 'generate:d6:component:info-file';
+    $this->answers = [
+      'Example',
+      'example',
+      'Some description',
+      'custom',
+      '6.x-1.0',
+    ];
+    $this->target = 'example.info';
+    $this->fixture = __DIR__ . '/_' . $this->target;
+
+    parent::setUp();
+  }
+
+}
