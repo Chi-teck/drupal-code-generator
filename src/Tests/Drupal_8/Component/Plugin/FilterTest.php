7776b093d52288df10af9a907220a2725eca072c
Chi <Chi@556138.no-reply.drupal.org>
2015-07-18 20:32:49 +0500

    Added d8 filter plugin test.

---
+++ src/Tests/Drupal_8/Component/Plugin/FilterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DrupalCodeGenerator\Tests\Drupal_8\Component\Plugin;
+
+use DrupalCodeGenerator\Tests\GeneratorTestCase;
+use DrupalCodeGenerator\Command\Drupal_8\Component\Plugin\Filter;
+
+class FilterTest extends GeneratorTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp () {
+    $this->command = new Filter();
+    $this->commandName = 'generate:d8:component:plugin:filter';
+    $this->answers = [
+      'Filter example',
+      'filter_example',
+      'Example of filter plugin.',
+      'filter_example',
+    ];
+    $this->target = 'FilterExample.php';
+    $this->fixture = __DIR__ . '/_' . $this->target;
+
+    parent::setUp();
+  }
+
+}
