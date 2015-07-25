77f1de0b13a4ec2385c4678046a511d9bc0f166a
Chi <Chi@556138.no-reply.drupal.org>
2015-07-25 11:44:17 +0500

    Moved content type plugin test to subdirectory.

--- src/Tests/Drupal_7/Component/CToolsPlugin/content_type/ContentTypeTest.php
+++ src/Tests/Drupal_7/Component/CToolsPlugin/content_type/ContentTypeTest.php
@@ -1,6 +1,5 @@
 <?php
 
-// @TODO: Create CtoolsPlugin name space.
 namespace DrupalCodeGenerator\Tests\Drupal_7\Component;
 
 use DrupalCodeGenerator\Tests\GeneratorTestCase;
@@ -15,12 +14,12 @@ class CtoolsPluginContentTypeTest extends GeneratorTestCase {
     $this->command = new ContentType();
     $this->commandName = 'generate:d7:component:ctools-plugin:content-type';
     $this->answers = [
-      'Node title',
-      'node_title',
+      'Example',
+      'example',
       'Some description',
       'custom',
     ];
-    $this->target = 'node_title.inc';
+    $this->target = 'example.inc';
     $this->fixture = __DIR__ . '/_' . $this->target;
 
     parent::setUp();
