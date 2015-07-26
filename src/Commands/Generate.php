259cd98e9ef96d1d99388671c93c7097dd5f48f5
Chi-teck <Chi-teck@users.noreply.github.com>
2015-07-26 17:30:29 +0500

    Added menu generation.

--- src/Commands/Generate.php
+++ src/Commands/Generate.php
@@ -18,50 +18,8 @@ class Generate extends Command {
   protected $name = 'generate';
   protected $description = 'Generate code';
   protected $activeMenuItems = [];
-  protected $menuTree =  [
-    'Drupal 8' => [
-      'Module',
-      'Theme',
-      'Installation profile',
-      'Component' => [
-        'Plugin' => [
-          'Filter',
-        ],
-        'YML file' => [
-          'libraries.yml',
-          'routing.yml',
-          'services.yml'
-        ]
-      ],
-    ],
-    'Drupal 7' => [
-      'Module',
-      'Theme',
-      'Component' => [
-        'settings.php',
-        'Info file',
-        'Install file',
-        'Module file',
-        'Js file',
-        'Views' => [],
-        'CTools Plugin' => [
-          'Content type',
-          'Access',
-          'Relationship',
-        ],
-      ],
-    ],
-    'Drupal 6' => [
-      'Component' => [
-        'Info file',
-      ],
-    ],
-    'Other' => [
-      'Drush command',
-      'Apache virtual host',
-      'HTML page'
-    ]
-  ];
+  protected $menuTree;
+  protected $activeMenuItem = 'generate';
 
 
   /**
@@ -103,34 +61,43 @@ class Generate extends Command {
    */
   protected function selectGenerator(InputInterface $input, OutputInterface $output) {
 
-    // Find active subtree;
-    $active_tree = $this->menuTree;
-    foreach ($this->activeMenuItems as $menu_item) {
-      $menu_item = strip_tags($menu_item);
-      $active_tree = isset($active_tree[$menu_item]) ? $active_tree[$menu_item] : [];
+    // Build menu tree.
+    if (!$this->menuTree) {
+      $this->buildMenuTree();
     }
 
-    foreach ($active_tree as $index => $subtree) {
-      $menu[] = is_array($subtree) ? $index : "<comment>$subtree</comment>";
+    $tree = $this->menuTree;
+    foreach ($this->activeMenuItems as $menuItem) {
+      $tree = $tree[strip_tags($menuItem)];
     }
 
-    if (isset($menu)) {
+    if (is_array($tree)) {
 
-      // Zero key is used to move back into the parent menu item.
-      array_unshift($menu, '..');
-      if (!$this->activeMenuItems) {
-        unset($menu[0]);
+      // First menu item is used to move back into the parent menu item.
+      $tree = ['..' => NULL] + $tree;
+
+      $choices = [];
+      foreach ($tree as $menu_item => $subtree) {
+        // We build $choices as an associative array to be able to find
+        // later menu items by respective labels.
+        $choices[$menu_item] = $this->createMenuItemLabel($menu_item, is_array($subtree));
       }
 
       $question = new ChoiceQuestion(
         '<title>Select generator:</title>',
-        $menu
+        array_values($choices)
       );
       $question->setPrompt('  >>> ');
+      $answer_label = $this->getHelper('question')->ask($input, $output, $question);
 
-      $answer = $this->getHelper('question')->ask($input, $output, $question);
+      $answer = array_search($answer_label, $choices);
 
       if ($answer == '..') {
+        // Exit the application if the user choices zero key
+        // on the top level menu.
+        if (count($this->activeMenuItems) == 0) {
+          exit;
+        }
         array_pop($this->activeMenuItems);
       }
       else {
@@ -140,13 +107,76 @@ class Generate extends Command {
       return $this->selectGenerator($input, $output);
     }
     else {
-      $generator = strip_tags(implode(':', $this->activeMenuItems));
-      $generator = strtolower($generator);
-      $generator = str_replace('drupal ', 'd', $generator);
-      $generator = str_replace(' ', '-', $generator);
-      return 'generate:' . $generator;
+      return 'generate:' . implode(':', $this->activeMenuItems);
     }
 
   }
 
+  protected function createMenuItemLabel($menu_item, $comment) {
+    $label = preg_replace('#^d([6-8])$#', 'Drupal_$1', $menu_item);
+    $label = ucfirst($label);
+    $label = str_replace(['-', '_'], ' ', $label);
+    return $comment ? "<comment>$label</comment>" : $label;
+  }
+
+
+  /**
+   *
+   */
+  protected function buildMenuTree() {
+    $this->menuTree = [];
+    $commands = $this->getApplication()->all();
+    foreach ($commands as $index => $command) {
+      $command_name = $command->getName();
+      $command_subnames = explode(':', $command_name);
+      if ($command_subnames[0] == 'generate') {
+        unset($command_subnames[0]);
+        $this->arraySetNestedValue($this->menuTree, $command_subnames, TRUE);
+      }
+    }
+
+    $this->recursiveKsort($this->menuTree);
+
+  }
+
+  /**
+   *
+   */
+  protected function recursiveKsort(array &$array) {
+    foreach ($array as &$value) {
+      if (is_array($value)) {
+        $this->recursiveKsort($value);
+      }
+    }
+    return ksort($array);
+  }
+
+
+  /**
+   * Sets a value in a nested array with variable depth.
+   *
+   * @param array $array
+   *   A reference to the array to modify.
+   * @param array $parents
+   *   An array of parent keys, starting with the outermost key.
+   * @param mixed $value
+   *   The value to set.
+   *
+   * @see https://api.drupal.org/api/drupal/includes!common.inc/function/drupal_array_set_nested_value/7
+   */
+  protected function arraySetNestedValue(array &$array, array $parents, $value) {
+    $ref = &$array;
+    foreach ($parents as $parent) {
+      if (isset($ref) && !is_array($ref)) {
+        $ref = array();
+      }
+      $ref = &$ref[$parent];
+    }
+    if (!isset($ref)) {
+      $ref = $value;
+    }
+  }
+
 }
+
+
