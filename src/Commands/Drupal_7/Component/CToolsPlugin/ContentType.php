55a3750af4a9f1413e662b029395464428efa54d
Chi-teck <Chi-teck@users.noreply.github.com>
2015-07-26 12:39:50 +0500

    Code style fixes.

--- src/Commands/Drupal_7/Component/CToolsPlugin/ContentType.php
+++ src/Commands/Drupal_7/Component/CToolsPlugin/ContentType.php
@@ -2,11 +2,6 @@
 
 namespace DrupalCodeGenerator\Commands\Drupal_7\Component\CToolsPlugin;
 
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use DrupalCodeGenerator\Commands\BaseGenerator;
-use Symfony\Component\Console\Question\ChoiceQuestion;
-
 /**
  * Implements generate:d7:component:ctools-plugin:content-type command.
  */
