dc7cfd223dcb5eaa373a525f3f0e7e5adef7e4cc
Chi-teck <Chi-teck@users.noreply.github.com>
2015-08-03 12:17:16 +0500

    Added PHP compactor.

---
+++ src/Box/PhpCompactor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DrupalCodeGenerator\Box;
+
+use Herrera\Box\Compactor\Compactor;
+
+/**
+ * A PHP source code compactor.
+ */
+class PhpCompactor extends Compactor {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected $extensions = ['php'];
+
+  /**
+   * {@inheritDoc}
+   */
+  public function compact($contents) {
+    // php_strip_whitespace() takes file name as argument so we have
+    // to save the contents to a temporary file.
+    $temp_file = tempnam(sys_get_temp_dir(), 'dcg-');
+    file_put_contents($temp_file, $contents);
+    $contents = php_strip_whitespace($temp_file);
+    unlink($temp_file);
+    return $contents;
+  }
+
+}
