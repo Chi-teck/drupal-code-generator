a62c187edafbf60c7a6cb1b604d4263f6cea96bf
Chi-teck <Chi-teck@users.noreply.github.com>
2015-08-07 20:33:39 +0500

    Added generators discovery test.

---
+++ src/Tests/GeneratorsDiscoveryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DrupalCodeGenerator\Tests;
+
+use DrupalCodeGenerator\Commands\Other;
+use DrupalCodeGenerator\GeneratorsDiscovery;
+use DrupalCodeGenerator\Commands;
+use Symfony\Component\Filesystem\Filesystem;
+
+class GeneratorsDiscoveryTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Test callback.
+   */
+  public function testExecute() {
+    $filesystem = new Filesystem();
+    $twig_loader = new \Twig_Loader_Filesystem(DCG_ROOT . '/src/Resources/templates');
+    $twig = new \Twig_Environment($twig_loader);
+    $discovery = new GeneratorsDiscovery([DCG_ROOT . '/src/Commands'], $filesystem, $twig);
+    $generators = $discovery->getGenerators();
+    foreach ($generators as $generator) {
+      $this->assertInstanceOf('DrupalCodeGenerator\Commands\BaseGenerator', $generator);
+    }
+    $this->assertCount(15, $generators);
+  }
+
+}
