bc2948b9178a5b1325778232647c26d313111603
Chi-teck <Chi-teck@users.noreply.github.com>
2015-11-12 11:47:00 +0300

    Added d8:javascript command.

---
+++ src/Tests/Generators/Drupal_8/_foo.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * Foo behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Example behavior description.
+   */
+  Drupal.behaviors.fooExample = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+}(jQuery, Drupal));
+
--- src/Tests/GeneratorsDiscoveryTest.php
+++ src/Tests/GeneratorsDiscoveryTest.php
@@ -12,7 +12,7 @@ use Symfony\Component\Filesystem\Filesystem;
  */
 class GeneratorsDiscoveryTest extends \PHPUnit_Framework_TestCase {
 
-  const TOTAL_GENERATORS = 33;
+  const TOTAL_GENERATORS = 34;
 
   /**
    * Test callback.
