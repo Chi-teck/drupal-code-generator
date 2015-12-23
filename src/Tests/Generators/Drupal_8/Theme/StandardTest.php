9071d89e794a9bdf3b3062c09dd2896fdf8a6ed2
Chi-teck <Chi-teck@users.noreply.github.com>
2015-12-23 16:50:12 +0300

    Added test for d8:theme:standard command.

---
+++ src/Tests/Generators/Drupal_8/Theme/StandardTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace DrupalCodeGenerator\Tests\Drupal_8\Theme;
+
+use DrupalCodeGenerator\Tests\GeneratorTestCase;
+
+/**
+ * Test for d8:theme:standard command.
+ */
+class StandardTest extends GeneratorTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    $this->class = 'Drupal_8\Theme\Standard';
+    $this->answers = [
+      'Foo',
+      'foo',
+      'classy',
+      'Description',
+      'Custom',
+      '8.x-1.0',
+    ];
+    parent::setUp();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testExecute() {
+
+    $this->execute();
+
+    $lines[] = 'The following files and directories have been created:';
+    $lines[] = '- foo/foo.info.yml';
+    $lines[] = '- foo/foo.libraries.yml';
+    $lines[] = '- foo/foo.theme';
+    $lines[] = '- foo/js/foo.js';
+    $lines[] = '- foo/templates';
+    $lines[] = '- foo/images';
+    $lines[] = '- foo/css/base/elements.css';
+    $lines[] = '- foo/css/components/block.css';
+    $lines[] = '- foo/css/components/breadcrumb.css';
+    $lines[] = '- foo/css/components/field.css';
+    $lines[] = '- foo/css/components/form.css';
+    $lines[] = '- foo/css/components/header.css';
+    $lines[] = '- foo/css/components/menu.css';
+    $lines[] = '- foo/css/components/messages.css';
+    $lines[] = '- foo/css/components/node.css';
+    $lines[] = '- foo/css/components/sidebar.css';
+    $lines[] = '- foo/css/components/table.css';
+    $lines[] = '- foo/css/components/tabs.css';
+    $lines[] = '- foo/css/components/buttons.css';
+    $lines[] = '- foo/css/layouts/layout.css';
+    $lines[] = '- foo/css/theme/print.css';
+    $lines[] = '';
+
+    $output = implode("\n", $lines);
+    $this->assertEquals($output, $this->commandTester->getDisplay());
+
+  }
+
+}
