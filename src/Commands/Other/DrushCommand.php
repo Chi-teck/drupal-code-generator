ccdb3c01b422dc2157394f46129ccdd577e03586
Chi-teck <Chi-teck@users.noreply.github.com>
2016-07-29 15:02:59 +0300

    Cleanup drush-command generator.

--- src/Commands/Other/DrushCommand.php
+++ src/Commands/Other/DrushCommand.php
@@ -19,23 +19,40 @@ class DrushCommand extends BaseGenerator {
    * {@inheritdoc}
    */
   protected function interact(InputInterface $input, OutputInterface $output) {
+
     $questions = [
-      'name' => ['Module name', [$this, 'defaultName']],
-      'machine_name' => ['Module machine name', [$this, 'defaultMachineName']],
-      'command_name' => ['Command name', [$this, 'defaultMachineName']],
+      'command_name' => ['Command name', ''],
       'alias' => ['Command alias', [$this, 'defaultAlias']],
-      'description' => ['Command description', 'TODO: Write description for the command'],
+      'description' => ['Command description', 'Command description.'],
       'argument' => ['Argument name', 'foo'],
       'option' => ['Option name', 'bar'],
+      'command_file' => ['Command name', [$this, 'defaultCommandFile']],
     ];
 
     $vars = $this->collectVars($input, $output, $questions);
 
-    $this->files[$vars['machine_name'] . '.drush.inc'] = $this->render('other/drush-command.twig', $vars);
+    list($vars['command_file_prefix']) = explode('.drush.inc', $vars['command_file']);
+
+    // Command callback name pattern gets shorter if command file name matches
+    // command name.
+    $vars['command_callback_suffix'] = $vars['command_file_prefix'] == str_replace('-', '_', $vars['command_name']) ?
+      $vars['command_file_prefix'] : $vars['command_file_prefix'] . '_' . $vars['command_name'];
+
+    $this->files[$vars['command_file']] = $this->render('other/drush-command.twig', $vars);
   }
 
   /**
-   * Returns default answer for alias question.
+   * Returns command file name.
+   */
+  protected function defaultCommandFile($vars) {
+    // The suggestion depends on whether the command global or local.
+    $prefix = $this->directoryBaseName == 'drush' || $this->directoryBaseName == '.drush' ?
+      $vars['command_name'] : $this->directoryBaseName;
+    return str_replace('-', '_', $prefix) . '.drush.inc';
+  }
+
+  /**
+   * Returns command alias.
    */
   protected function defaultAlias($vars) {
     return substr($vars['command_name'], 0, 3);
