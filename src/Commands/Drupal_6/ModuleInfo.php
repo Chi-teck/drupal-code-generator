1015bc9932f9d2885c566519eec3c3e3a71c20c7
Chi-teck <Chi-teck@users.noreply.github.com>
2016-12-17 10:59:05 +0300

    Cleanup d6:module-info command.

--- src/Commands/Drupal_6/ModuleInfo.php
+++ src/Commands/Drupal_6/ModuleInfo.php
@@ -3,6 +3,7 @@
 namespace DrupalCodeGenerator\Commands\Drupal_6;
 
 use DrupalCodeGenerator\Commands\BaseGenerator;
+use DrupalCodeGenerator\Commands\Utils;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -18,14 +19,11 @@ class ModuleInfo extends BaseGenerator {
    * {@inheritdoc}
    */
   protected function interact(InputInterface $input, OutputInterface $output) {
-    $questions = [
-      'name' => ['Module name'],
-      'machine_name' => ['Module machine name'],
+    $questions = Utils::defaultQuestions() + [
       'description' => ['Module description', 'TODO: Write description for the module'],
       'package' => ['Package', 'custom'],
       'version' => ['Version', '6.x-1.0-dev'],
     ];
-
     $vars = $this->collectVars($input, $output, $questions);
     $this->files[$vars['machine_name'] . '.info'] = $this->render('d6/module-info.twig', $vars);
   }
