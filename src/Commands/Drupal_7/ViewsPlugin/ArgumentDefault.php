8210fc3785847a43e994d14307ecc965a73effa0
Chi-teck <Chi-teck@users.noreply.github.com>
2016-12-17 10:35:04 +0300

    Cleanup d7:views-plugin:argument-default generator.

--- src/Commands/Drupal_7/ViewsPlugin/ArgumentDefault.php
+++ src/Commands/Drupal_7/ViewsPlugin/ArgumentDefault.php
@@ -2,9 +2,10 @@
 
 namespace DrupalCodeGenerator\Commands\Drupal_7\ViewsPlugin;
 
+use DrupalCodeGenerator\Commands\BaseGenerator;
+use DrupalCodeGenerator\Commands\Utils;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use DrupalCodeGenerator\Commands\BaseGenerator;
 
 /**
  * Implements d7:views-plugin:argument-default command.
@@ -18,28 +19,19 @@ class ArgumentDefault extends BaseGenerator {
    * {@inheritdoc}
    */
   protected function interact(InputInterface $input, OutputInterface $output) {
-
     $questions = [
       'name' => ['Module name'],
       'machine_name' => ['Module machine name'],
       'plugin_name' => ['Plugin name', 'Example'],
       'plugin_machine_name' => [
-        'Plugin machine name', [$this, 'defaultPluginMachineName'],
+        'Plugin machine name',
+        function ($vars) {
+          return Utils::human2machine($vars['plugin_name']);
+        },
       ],
     ];
-
     $vars = $this->collectVars($input, $output, $questions);
-    $vars['project_type'] = 'module';
-
     $this->files['views_plugin_argument_' . $vars['plugin_machine_name'] . '.inc'] = $this->render('d7/views-argument-default.twig', $vars);
-
-  }
-
-  /**
-   * Returns default value for the plugin machine name question.
-   */
-  protected function defaultPluginMachineName($vars) {
-    return $this->defaultMachineName(['name' => $vars['plugin_name']]);
   }
 
 }
