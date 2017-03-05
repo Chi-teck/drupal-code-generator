b19c63c31aa0f3a95d142097c401e8c35d3b881d
Chi-teck <Chi-teck@users.noreply.github.com>
2017-03-05 19:24:59 +0300

    Added views field plugin generator.

---
+++ src/Commands/Drupal_8/Plugin/Views/Field.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DrupalCodeGenerator\Commands\Drupal_8\Plugin\Views;
+
+use DrupalCodeGenerator\Commands\BaseGenerator;
+use DrupalCodeGenerator\Utils;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Implements d8:plugin:views:field command.
+ */
+class Field extends BaseGenerator {
+
+  protected $name = 'd8:plugin:views:field';
+  protected $description = 'Generates views field plugin';
+  protected $alias = 'views-field';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function interact(InputInterface $input, OutputInterface $output) {
+    $questions = Utils::defaultQuestions() + [
+      'plugin_label' => ['Plugin label', 'Example'],
+      'plugin_id' => ['Plugin id'],
+    ];
+
+    $vars = $this->collectVars($input, $output, $questions);
+    $vars['class'] = Utils::camelize($vars['plugin_label']);
+
+    $path = 'src/Plugin/views/field/' . $vars['class'] . '.php';
+    $this->files[$path] = $this->render('d8/plugin/views/field.twig', $vars);
+  }
+
+}
