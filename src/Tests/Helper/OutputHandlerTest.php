3e1a200cb258eaaa695b8dbd0145c2b29dc337ae
Chi-teck <Chi-teck@users.noreply.github.com>
2017-05-28 15:44:36 +0500

    Added assertion for output handler getName() method.

--- src/Tests/Helper/OutputHandlerTest.php
+++ src/Tests/Helper/OutputHandlerTest.php
@@ -15,8 +15,8 @@ class OutputHandlerTest extends TestCase {
    * Test callback.
    */
   public function testOutputHandler() {
-    $output = new BufferedOutput();
     $handler = new OutputHandler();
+    $output = new BufferedOutput();
 
     $handler->printSummary($output, ['aaa', 'bbb', 'ccc']);
     $expected_output = "<title>The following directories and files have been created or updated:</title>\n- aaa\n- bbb\n- ccc\n";
@@ -24,6 +24,8 @@ class OutputHandlerTest extends TestCase {
 
     $handler->printSummary($output, []);
     $this->assertEquals($output->fetch(), '');
+
+    $this->assertEquals($handler->getName(), 'dcg_output_handler');
   }
 
 }
