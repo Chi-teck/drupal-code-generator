fabaf4a4048ef7db4837632c31f2a777f4b299c0
Chi-teck <Chi-teck@users.noreply.github.com>
2017-05-28 15:18:37 +0500

    Added test for output handler.

--- src/Tests/Helper/OutputHandlerTest.php
+++ src/Tests/Helper/OutputHandlerTest.php
@@ -2,7 +2,9 @@
 
 namespace DrupalCodeGenerator\Tests\Helper;
 
+use DrupalCodeGenerator\Helper\OutputHandler;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
  * A test for output handler.
@@ -13,7 +15,15 @@ class OutputHandlerTest extends TestCase {
    * Test callback.
    */
   public function testOutputHandler() {
-    $this->markTestIncomplete('This test has not been implemented yet.');
+    $output = new BufferedOutput();
+    $handler = new OutputHandler();
+
+    $handler->printSummary($output, ['aaa', 'bbb', 'ccc']);
+    $expected_output = "<title>The following directories and files have been created or updated:</title>\n- aaa\n- bbb\n- ccc\n";
+    $this->assertEquals($output->fetch(), $expected_output);
+
+    $handler->printSummary($output, []);
+    $this->assertEquals($output->fetch(), '');
   }
 
 }
