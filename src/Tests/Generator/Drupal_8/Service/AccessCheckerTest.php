5252cfbd0ebb2dcbff553a3be98d06645d57dce5
Chi-teck <Chi-teck@users.noreply.github.com>
2017-06-03 13:02:11 +0500

    Added access_checker template.

--- src/Tests/Generator/Drupal_8/Service/AccessCheckerTest.php
+++ src/Tests/Generator/Drupal_8/Service/AccessCheckerTest.php
@@ -20,6 +20,7 @@ class AccessCheckerTest extends GeneratorTestCase {
 
   protected $fixtures = [
     'src/Access/FooAccessChecker.php' => __DIR__ . '/_access_checker.php',
+    'example.services.yml' => __DIR__ . '/_access_checker.services.yml',
   ];
 
 }
