a7bee3bbe22e5e6afe4d1dd252fb938a8447ef7f
Chi-teck <Chi-teck@users.noreply.github.com>
2017-06-25 12:34:02 +0500

    Added SUT test runner.

---
+++ scripts/sut/form/foo/tests/src/Functional/FormTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\Tests\foo\Functional;
+
+use Drupal\Core\Database\Database;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Form test.
+ *
+ * @group DCG
+ */
+class FormTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['foo'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $admin_user = $this->drupalCreateUser(['access administration pages']);
+    $this->drupalLogin($admin_user);
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testSimpleForm() {
+    $this->drupalGet('admin/config/foo/simple');
+    $prefix = '//form[@id="foo-simple"]';
+    $this->assertXpath($prefix . '//label[text() = "Message"]');
+    $this->assertXpath($prefix . '//textarea[@name = "message"]');
+    $this->assertXpath($prefix . '//input[@type = "submit" and @value = "Save"]');
+
+    $edit = [
+      'message' => 123456789,
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Save');
+    $this->assertXpath('//div[contains(., "Message should be at least 10 characters.")]');
+
+    $edit = [
+      'message' => 1234567890,
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Save');
+    $this->assertXpath('//div[contains(., "The message has been sent.")]');
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testConfigForm() {
+    $this->drupalGet('admin/config/foo/settings');
+    $prefix = '//form[@id="foo-settings"]';
+    $this->assertXpath($prefix . '//label[text() = "Example"]');
+    $this->assertXpath($prefix . '//input[@name = "example" and @value="none"]');
+    $this->assertXpath($prefix . '//input[@type = "submit" and @value = "Save configuration"]');
+    $edit = [
+      'example' => 'Some text.',
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Save configuration');
+    $this->assertXpath('//div[contains(., "The value is not correct.")]');
+
+    $edit = [
+      'example' => 'example',
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Save configuration');
+    $this->assertXpath('//div[contains(., "The configuration options have been saved.")]');
+    $this->assertXpath($prefix . '//input[@name = "example" and @value="example"]');
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testConfirmForm() {
+    $this->drupalGet('admin/config/foo/confirm');
+    $this->assertXpath('//h1[text() = "Are you sure you want to delete all examples?"]');
+    $this->assertXpath('//form[@id="foo-confirm" and contains(., "This action cannot be undone.")]');
+    $this->clickLink('Cancel');
+    $this->assertSession()->addressEquals('/admin/config/foo/settings');
+
+    // Create a table for testing form submission.
+    $table['fields']['id']['type'] = 'int';
+    Database::getConnection()->schema()->createTable('examples', $table);
+    $this->drupalPostForm('admin/config/foo/confirm', [], 'Confirm');
+    $this->assertXpath('//div[contains(., "The examples have been deleted.")]');
+  }
+
+  /**
+   * Checks that an element exists on the current page.
+   *
+   * @param string $selector
+   *   The XPath identifying the element to check.
+   */
+  protected function assertXpath($selector) {
+    $this->assertSession()->elementExists('xpath', $selector);
+  }
+
+}
---
+++ scripts/sut/run.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -e
+function dcg_on_exit {
+  if [ $? -eq 0 ] ; then
+    echo -e "\n\e[0;42m SUCCESS \e[0m"
+  else
+    echo -e "\n\e[0;41m FAIL \e[0m"
+  fi
+}
+trap dcg_on_exit EXIT
+
+SELF_PATH=$(dirname $0)
+DRUPAL_PATH=${DRUPAL_PATH:-/tmp/dcg_sut}
+DRUPAL_VERSION=${DRUPAL_VERSION:-8.4.x-dev}
+SUT_HOST=${SUT_HOST:-127.0.0.1}
+SUT_PORT=${SUT_PORT:-8085}
+DCG=${DCG:-/var/www/dcg/bin/dcg}
+
+echo --------------------------------------
+echo ' DRUPAL_PATH:   ' $DRUPAL_PATH
+echo ' DRUPAL_VERSION:' $DRUPAL_VERSION
+echo ' DRUPAL_HOST:   ' $SUT_HOST
+echo ' DRUPAL_PORT:   ' $SUT_PORT
+echo ' DCG:           ' $DCG
+echo --------------------------------------
+
+function dcg_drush {
+  $DRUPAL_PATH/vendor/bin/drush -r $DRUPAL_PATH -y $@
+}
+
+function dcg_phpcs {
+  $DRUPAL_PATH/vendor/bin/phpcs --standard=Drupal,DrupalPractice $@
+}
+
+function dcg_phpunit {
+  SIMPLETEST_BASE_URL=http://$SUT_HOST:$SUT_PORT \
+  SIMPLETEST_DB=sqlite://sites/default/files/.ht.sqlite#st_ \
+  $DRUPAL_PATH/vendor/bin/phpunit \
+  -c $DRUPAL_PATH/core $@
+}
+
+# Assuming that global Drush is not installed.
+sudo rm -rf $DRUPAL_PATH
+wget -P /tmp https://ftp.drupal.org/files/projects/drupal-$DRUPAL_VERSION.tar.gz
+tar xf /tmp/drupal-$DRUPAL_VERSION.tar.gz -C /tmp
+mv /tmp/drupal-$DRUPAL_VERSION $DRUPAL_PATH
+rm /tmp/drupal-$DRUPAL_VERSION.tar.gz
+composer -d=$DRUPAL_PATH require drush/drush:dev-master
+mkdir -m 777 $DRUPAL_PATH/sites/default/files
+composer -d=$DRUPAL_PATH update squizlabs/php_codesniffer
+$DRUPAL_PATH/vendor/bin/phpcs --config-set installed_paths $DRUPAL_PATH/vendor/drupal/coder/coder_sniffer
+dcg_drush si minimal --db-url=sqlite://sites/default/files/.ht.sqlite
+
+IS_RUNNING=$(netstat -lnt | awk "/$SUT_HOST:$SUT_PORT/ { print \"FOUND\" }")
+if [ -z "$IS_RUNNING" ]; then
+  echo Staring server...
+  php -S $SUT_HOST:$SUT_PORT -t $DRUPAL_PATH &>/dev/null &
+fi
+
+# Run tests.
+echo -e "\e[30;43m -= Form test =- \e[0m\n"
+source $SELF_PATH/form/test.sh
