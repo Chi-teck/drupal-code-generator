8c3abc4403407cd1dd5f5e4bbdba5f9326c02491
Chi-teck <Chi-teck@users.noreply.github.com>
2017-06-26 08:32:33 +0500

    Added controller route template.

--- src/Command/Drupal_8/Controller.php
+++ src/Command/Drupal_8/Controller.php
@@ -6,6 +6,8 @@ use DrupalCodeGenerator\Command\BaseGenerator;
 use DrupalCodeGenerator\Utils;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
 
 /**
  * Implements d8:controller command.
@@ -28,7 +30,22 @@ class Controller extends BaseGenerator {
         },
       ],
     ];
+    $questions['route'] = new ConfirmationQuestion('Would you like to create a route for this controller?');
     $vars = $this->collectVars($input, $output, $questions);
+
+    if ($vars['route']) {
+      $route_path = '/' . str_replace('_', '-', $vars['machine_name']) . '/example';
+      $route_questions['route_name'] = new Question('Route name', $vars['machine_name'] . '.example');
+      $route_questions['route_path'] = new Question('Route path', $route_path);
+      $route_questions['route_title'] = new Question('Route title', 'Example');
+      $route_questions['route_permission'] = new Question('Route permission', 'access content');
+      $vars += $this->collectVars($input, $output, $route_questions);
+      $this->files[$vars['machine_name'] . '.routing.yml'] = [
+        'content' => $this->render('d8/controller-route.twig', $vars),
+        'action' => 'append',
+      ];
+    }
+
     $path = 'src/Controller/' . $vars['class'] . '.php';
     $this->files[$path] = $this->render('d8/controller.twig', $vars);
   }
