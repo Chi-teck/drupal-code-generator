582f2f099b6eb6001a12496677266143c1a1fa40
Chi-teck <Chi-teck@users.noreply.github.com>
2017-06-30 13:10:42 +0500

    Cleanup output handler.

--- src/Tests/Helper/OutputHandlerTest.php
+++ src/Tests/Helper/OutputHandlerTest.php
@@ -18,14 +18,24 @@ class OutputHandlerTest extends TestCase {
     $handler = new OutputHandler();
     $output = new BufferedOutput();
 
-    $handler->printSummary($output, ['aaa', 'bbb', 'ccc']);
-    $expected_output = "<title>The following directories and files have been created or updated:</title>\n- aaa\n- bbb\n- ccc\n";
-    $this->assertEquals($output->fetch(), $expected_output);
+    $handler->printSummary(
+      $output,
+      ['bbb/eee/ggg', 'aaa/ddd', 'ccc', 'aaa', 'bbb/fff']
+    );
+    $expected_output = "<title>The following directories and files have been created or updated:</title>\n";
+    $expected_output .= implode([
+      "- aaa\n",
+      "- ccc\n",
+      "- aaa/ddd\n",
+      "- bbb/fff\n",
+      "- bbb/eee/ggg\n",
+    ]);
+    self::assertEquals($expected_output, $output->fetch());
 
     $handler->printSummary($output, []);
-    $this->assertEquals($output->fetch(), '');
+    self::assertEquals('', $output->fetch());
 
-    $this->assertEquals($handler->getName(), 'dcg_output_handler');
+    self::assertEquals('dcg_output_handler', $handler->getName());
   }
 
 }
