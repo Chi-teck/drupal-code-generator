1791fa8bb71d713ba8af1fddee0622b643dcfb65
Chi-teck <Chi-teck@users.noreply.github.com>
2017-09-17 11:28:21 +0500

    Added SUT test for template generator.

---
+++ scripts/sut/bar/tests/src/Kernel/TemplateTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\Tests\bar\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Template test.
+ *
+ * @group DCG
+ */
+class TestTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['system', 'bar'];
+
+  /**
+   * Test callback.
+   */
+  public function testTemplateRendering() {
+    $build = ['#theme' => 'example'];
+    $this->assertEquals(
+      "<div class=\"wrapper-class\">\n  bar\n</div>\n",
+      $this->container->get('renderer')->renderRoot($build)
+    );
+  }
+
+}
