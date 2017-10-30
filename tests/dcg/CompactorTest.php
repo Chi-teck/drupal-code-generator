d1b9ead69357168c96575aa21b75cae2504a69c5
Chi-teck <Chi-teck@users.noreply.github.com>
2017-10-30 22:18:44 +0500

    Added test for PHP compactor.

---
+++ tests/dcg/CompactorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DrupalCodeGenerator\Tests;
+
+use DrupalCodeGenerator\Box\PhpCompactor;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * A test for PHP compactor.
+ */
+class CompactorTest extends TestCase {
+
+  /**
+   * Test callback.
+   */
+  public function testCompactor() {
+    // Define base class for PhpCompactor as it may not exist.
+    if (!class_exists('Herrera\Box\Compactor\Compactor')) {
+      eval('namespace Herrera\Box\Compactor; class Compactor {}');
+    }
+    $code = "<?php\n// Comment.\nif (TRUE) {\n  echo 'bar';\n}\n";
+    static::assertEquals(
+      "<?php\nif (TRUE) { echo 'bar'; } ",
+      ( new PhpCompactor())->compact($code)
+    );
+  }
+
+}
