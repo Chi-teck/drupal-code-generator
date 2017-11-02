2dfa98dd468bbf3e4b2ae498233a9b348903b58b
Chi-teck <Chi-teck@users.noreply.github.com>
2017-11-02 21:47:40 +0500

    Added test for DCG application.

--- tests/dcg/ApplicationTest.php
+++ tests/dcg/ApplicationTest.php
@@ -2,15 +2,10 @@
 
 namespace DrupalCodeGenerator\Tests;
 
-use DrupalCodeGenerator\Command\Navigation;
-use DrupalCodeGenerator\GeneratorDiscovery;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Helper\QuestionHelper;
-use Symfony\Component\Console\Tester\ApplicationTester;
-use Symfony\Component\Filesystem\Filesystem;
 
 /**
- * A test for global application options.
+ * A test for DCG application.
  */
 class ApplicationTest extends TestCase {
 
@@ -35,65 +30,16 @@ class ApplicationTest extends TestCase {
    */
   public function testApplication() {
 
-    $discovery = new GeneratorDiscovery(new Filesystem());
-    $generators = $discovery->getGenerators([DCG_ROOT . '/src/Command']);
+    $cmd = sprintf(DCG_ROOT . '/bin/dcg install --directory=%s -a\'%s\'', $this->directory, '{"name":"Foo", "machine_name":"foo"}');
+    exec($cmd, $output, $return);
 
-    putenv('SHELL_INTERACTIVE=1');
-    $application = dcg_create_application();
-    $application->addCommands($generators);
-    $application->setAutoExit(FALSE);
-
-    $navigation = new Navigation($generators);
-    $application->add($navigation);
-    $application->setDefaultCommand($navigation->getName());
-
-    $question_helper = new QuestionHelper();
-
-    $input_stream = static::getInputStream("0\n");
-    $question_helper->setInputStream($input_stream);
-
-    $helper_set = $application->getHelperSet();
-    $helper_set->set($question_helper);
-
-    $tester = new ApplicationTester($application);
-
-    // Test default command.
-    $tester->run([]);
-    $display = $tester->getDisplay();
-    $output = rtrim(preg_replace('/(>>> 0).*/', '$1', $display));
-    $expected_output = implode("\n", [
-      'Select generator:',
-      '  [0] ..',
-      '  [1] Drupal 7',
-      '  [2] Drupal 8',
-      '  [3] Other',
-      '  >>> 0',
-    ]);
+    $expected_output = [
+      'The following directories and files have been created or updated:',
+      '- foo.install',
+    ];
     static::assertEquals($expected_output, $output);
-
-    // Test version option.
-    $tester->run(['--version']);
-    static::assertEquals("Drupal Code Generator\n", $tester->getDisplay());
-
-    // Test directory and answers options.
-    $tester->run([
-      'command' => 'd8:install',
-      '-d' => $this->directory,
-      '-a' => '{"name":"Foo","machine_name":"foo"}',
-    ]);
-    $expected_output = "The following directories and files have been created or updated:\n- foo.install\n";
-    static::assertEquals($expected_output, $tester->getDisplay());
+    static::assertEquals(0, $return);
     static::assertFileExists($this->directory . '/foo.install');
-  }
-
-  /**
-   * Returns input stream.
-   */
-  protected static function getInputStream($input) {
-    $stream = fopen('php://memory', 'r+', FALSE);
-    fwrite($stream, $input);
-    rewind($stream);
-    return $stream;
   }
 
 }
