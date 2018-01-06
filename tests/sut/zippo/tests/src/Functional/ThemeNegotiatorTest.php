10794e9d7d8b2fb5ac123c09ecee231183bcd9de
Chi-teck <Chi-teck@users.noreply.github.com>
2018-01-06 14:39:52 +0500

    Added SUT test for theme negotiatio.

---
+++ tests/sut/zippo/tests/src/Functional/ThemeNegotiatorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\Tests\zippo\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test theme negotiator.
+ *
+ * @group DCG
+ */
+class ThemeNegotiatorTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['zippo'];
+
+  /**
+   * Test callback.
+   */
+  public function testThemeNegotiator() {
+    \Drupal::service('theme_handler')->install(['bartik']);
+
+    $options = ['query' => ['theme' => 'bartik']];
+    $pattern = '#/core/themes/bartik/#';
+    $assert_session = $this->assertSession();
+
+    $this->drupalGet('<front>', $options);
+    $assert_session->responseNotMatches($pattern);
+
+    $this->drupalGet('example', $options);
+    $assert_session->responseMatches($pattern);
+
+    $this->drupalGet('example');
+    $assert_session->responseNotMatches($pattern);
+  }
+
+}
