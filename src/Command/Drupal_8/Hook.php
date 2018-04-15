746c61c79f315e4cbb8aeed29375f94b774680f5
Chi-teck <Chi-teck@users.noreply.github.com>
2018-04-15 17:18:12 +0500

    Respect 'special' hook locations.

--- src/Command/Drupal_8/Hook.php
+++ src/Command/Drupal_8/Hook.php
@@ -33,16 +33,76 @@ class Hook extends BaseGenerator {
 
     $vars = $this->collectVars($input, $output, $questions);
 
-    $install_hooks = [
-      'install',
-      'uninstall',
-      'schema',
-      'requirements',
-      'update_N',
-      'update_last_removed',
+    // Most Drupal hooks are situated in a module file but some are not.
+    $special_hooks = [
+      'install' => [
+        'install',
+        'uninstall',
+        'schema',
+        'requirements',
+        'update_N',
+        'update_last_removed',
+      ],
+      // See views_hook_info().
+      'views.inc' => [
+        'views_data',
+        'views_data_alter',
+        'views_analyze',
+        'views_invalidate_cache',
+        'field_views_data',
+        'field_views_data_alter',
+        // See \Drupal\views\views::$plugins.
+        'views_plugins_access_alter',
+        'views_plugins_area_alter',
+        'views_plugins_argument_alter',
+        'views_plugins_argument_default_alter',
+        'views_plugins_argument_validator_alter',
+        'views_plugins_cache_alter',
+        'views_plugins_display_extender_alter',
+        'views_plugins_display_alter',
+        'views_plugins_exposed_form_alter',
+        'views_plugins_field_alter',
+        'views_plugins_filter_alter',
+        'views_plugins_join_alter',
+        'views_plugins_pager_alter',
+        'views_plugins_query_alter',
+        'views_plugins_relationship_alter',
+        'views_plugins_row_alter',
+        'views_plugins_sort_alter',
+        'views_plugins_style_alter',
+        'views_plugins_wizard_alter',
+      ],
+      'views_execution.inc' => [
+        'views_query_substitutions',
+        'views_form_substitutions',
+        'views_pre_view',
+        'views_pre_build',
+        'views_post_build',
+        'views_pre_execute',
+        'views_post_execute',
+        'views_pre_render',
+        'views_post_render',
+        'views_query_alter',
+      ],
+      // See system_hook_info().
+      'tokens.inc' => [
+        'token_info',
+        'token_info_alter',
+        'tokens',
+        'tokens_alter',
+      ],
+      'post_update.php' => [
+        'post_update_N',
+      ],
     ];
 
-    $file_type = in_array($vars['hook_name'], $install_hooks) ? 'install' : 'module';
+    $file_type = 'module';
+    foreach ($special_hooks as $group => $hooks) {
+      if (in_array($vars['hook_name'], $hooks)) {
+        $file_type = $group;
+        break;
+      }
+    }
 
     $this->addFile()
       ->path('{machine_name}.' . $file_type)
