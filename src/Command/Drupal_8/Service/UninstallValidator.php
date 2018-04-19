3cd906db7a1bd928586536ec50799939323c3efd
Chi-teck <Chi-teck@users.noreply.github.com>
2018-04-19 15:44:59 +0500

    Added uninstall validator generator.

---
+++ src/Command/Drupal_8/Service/UninstallValidator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DrupalCodeGenerator\Command\Drupal_8\Service;
+
+use DrupalCodeGenerator\Command\BaseGenerator;
+use DrupalCodeGenerator\Utils;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+/**
+ * Implements d8:service:uninstall-validator command.
+ */
+class UninstallValidator extends BaseGenerator {
+
+  protected $name = 'd8:service:uninstall-validator';
+  protected $description = 'Generates a uninstall validator service';
+  protected $alias = 'uninstall-validator';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function interact(InputInterface $input, OutputInterface $output) {
+    $questions = Utils::defaultQuestions();
+    $default_class = function ($vars) {
+      return Utils::camelize($vars['name'] . 'UninstallValidator');
+    };
+    $questions['class'] = new Question('Class', $default_class);
+    $this->collectVars($input, $output, $questions);
+
+    $this->addFile()
+      ->path('src/{class}.php')
+      ->template('d8/service/uninstall-validator.twig');
+
+    $this->addServicesFile()
+      ->template('d8/service/uninstall-validator.services.twig');
+  }
+
+}
