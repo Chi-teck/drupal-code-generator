006d6cc9067f3c075895978eca02ddf1c0412820
Chi-teck <Chi-teck@users.noreply.github.com>
2018-10-08 21:19:31 +0500

    Add fixtures for d7 module test

---
+++ tests/dcg/Generator/Drupal_7/_module.js
@@ -0,0 +1,17 @@
+/**
+ * Example behaviors.
+ */
+(function ($) {
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.example = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  }
+
+}(jQuery));
