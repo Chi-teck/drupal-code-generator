f562b49026942400b05b60eee53c930df9dac242
Chi-teck <Chi-teck@users.noreply.github.com>
2019-02-06 17:15:27 +0500

    Add migrate destination plugin generator

---
+++ src/Command/Drupal_8/Plugin/Migrate/Destination.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DrupalCodeGenerator\Command\Drupal_8\Plugin\Migrate;
+
+use DrupalCodeGenerator\Command\BaseGenerator;
+use DrupalCodeGenerator\Utils;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+/**
+ * Implements d8:plugin:migrate:destination command.
+ */
+class Destination extends BaseGenerator {
+
+  protected $name = 'd8:plugin:migrate:destination';
+  protected $description = 'Generates migrate destination plugin';
+  protected $alias = 'migrate-destination';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function interact(InputInterface $input, OutputInterface $output) {
+
+    $questions = Utils::defaultQuestions();
+    $questions['plugin_id'] = new Question('Plugin ID', '{machine_name}_example');
+    $questions['plugin_id']->setValidator([Utils::class, 'validateMachineName']);
+
+    $vars = &$this->collectVars($input, $output, $questions);
+
+    $unprefixed_plugin_id = preg_replace('/^' . $vars['machine_name'] . '_/', '', $vars['plugin_id']);
+    $vars['class'] = Utils::camelize($unprefixed_plugin_id);
+
+    $this->addFile()
+      ->path('src/Plugin/migrate/destination/{class}.php')
+      ->template('d8/plugin/migrate/destination.twig');
+  }
+
+}
---
+++ tests/dcg/Generator/Drupal_8/Plugin/Migrate/DestinationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DrupalCodeGenerator\Tests\Generator\Drupal_8\Plugin\Migrate;
+
+use DrupalCodeGenerator\Tests\Generator\GeneratorBaseTest;
+
+/**
+ * Test for d8:plugin:migrate:destination command.
+ */
+class DestinationTest extends GeneratorBaseTest {
+
+  protected $class = 'Drupal_8\Plugin\Migrate\Destination';
+
+  protected $interaction = [
+    'Module name [%default_name%]:' => 'Example',
+    'Module machine name [example]:' => 'example',
+    'Plugin ID [example_example]:' => 'example_bar',
+  ];
+
+  protected $fixtures = [
+    'src/Plugin/migrate/destination/Bar.php' => __DIR__ . '/_destination.php',
+  ];
+
+}
