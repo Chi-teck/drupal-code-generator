48e27b4ce3a192e00d78852854f8f27d4c64d9d6
Chi-teck <Chi-teck@users.noreply.github.com>
2019-05-04 19:08:14 +0500

    Add PSR-3 logger

---
+++ src/Logger.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace DrupalCodeGenerator;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\InvalidArgumentException;
+use Psr\Log\LogLevel;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * PSR-3 compliant console logger.
+ */
+class Logger extends AbstractLogger {
+
+  /**
+   * Console output.
+   *
+   * @var \Symfony\Component\Console\Output\OutputInterface
+   */
+  private $output;
+
+  /**
+   * Verbosity level map.
+   *
+   * @var array
+   */
+  private $verbosityLevelMap = [
+    LogLevel::EMERGENCY => OutputInterface::VERBOSITY_NORMAL,
+    LogLevel::ALERT => OutputInterface::VERBOSITY_NORMAL,
+    LogLevel::CRITICAL => OutputInterface::VERBOSITY_NORMAL,
+    LogLevel::ERROR => OutputInterface::VERBOSITY_NORMAL,
+    LogLevel::WARNING => OutputInterface::VERBOSITY_NORMAL,
+    LogLevel::NOTICE => OutputInterface::VERBOSITY_VERBOSE,
+    LogLevel::INFO => OutputInterface::VERBOSITY_VERY_VERBOSE,
+    LogLevel::DEBUG => OutputInterface::VERBOSITY_DEBUG,
+  ];
+
+  /**
+   * Logger constructor.
+   *
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   *   Console output.
+   */
+  public function __construct(OutputInterface $output) {
+    $this->output = $output;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function log($level, $message, array $context = []) :void {
+
+    if (!isset($this->verbosityLevelMap[$level])) {
+      throw new InvalidArgumentException("The log level \"$level\" does not exist.");
+    }
+
+    $output = $this->output;
+
+    // Write to the error output if necessary and available.
+    $error_levels = [
+      LogLevel::EMERGENCY,
+      LogLevel::ALERT,
+      LogLevel::CRITICAL,
+      LogLevel::ERROR,
+    ];
+    if (in_array($level, $error_levels)) {
+      if ($this->output instanceof ConsoleOutputInterface) {
+        /** @var \Symfony\Component\Console\Output\ConsoleOutputInterface $output */
+        $output = $output->getErrorOutput();
+      }
+    }
+
+    // The if condition check isn't necessary -- it's the same one that $output
+    // will do internally anyway. We only do it for efficiency here as the
+    // message formatting is relatively expensive.
+    if ($output->getVerbosity() >= $this->verbosityLevelMap[$level]) {
+
+      switch ($level) {
+        case LogLevel::EMERGENCY;
+        case LogLevel::ALERT;
+        case LogLevel::CRITICAL;
+        case LogLevel::ERROR;
+          $label = "<fg=red;options=bold;>$level</>";
+          break;
+
+        case LogLevel::WARNING;
+          $label = "<fg=yellow;options=bold;>$level</>";
+          break;
+
+        case LogLevel::NOTICE;
+        case LogLevel::INFO;
+          $label = "<fg=green;options=bold;>$level</>";
+          break;
+
+        case LogLevel::DEBUG;
+          $label = "<fg=cyan;options=bold;>$level</>";
+          break;
+      }
+
+      $formatted_message = sprintf('[%s] %s', $label, $this->interpolate($message, $context));
+      $output->writeln($formatted_message, $this->verbosityLevelMap[$level]);
+    }
+  }
+
+  /**
+   * Interpolates context values into the message placeholders.
+   *
+   * @see \Symfony\Component\Console\Logger::interpolate()
+   */
+  private function interpolate(string $message, array $context) :string {
+    if (FALSE === strpos($message, '{')) {
+      return $message;
+    }
+
+    $replacements = [];
+    foreach ($context as $key => $value) {
+      if (NULL === $value || is_scalar($value) || (\is_object($value) && method_exists($value, '__toString'))) {
+        $replacements["{{$key}}"] = $value;
+      }
+      elseif ($value instanceof \DateTimeInterface) {
+        $replacements["{{$key}}"] = $value->format(\DateTime::RFC3339);
+      }
+      elseif (\is_object($value)) {
+        $replacements["{{$key}}"] = '[object ' . \get_class($value) . ']';
+      }
+      else {
+        $replacements["{{$key}}"] = '[' . \gettype($value) . ']';
+      }
+    }
+
+    return strtr($message, $replacements);
+  }
+
+}
---
+++ tests/dcg/LoggerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace DrupalCodeGenerator\Tests;
+
+use DrupalCodeGenerator\Logger;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\InvalidArgumentException;
+use Psr\Log\LogLevel;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * A test for console logger.
+ */
+class LoggerTest extends TestCase {
+
+  /**
+   * Test callback.
+   */
+  public function testLogger() {
+
+    $output = new BufferedOutput(OutputInterface::VERBOSITY_NORMAL, TRUE);
+    $logger = new Logger($output);
+
+    $logger->emergency('Example');
+    self::assertEquals("[\e[31;1memergency\e[39;22m] Example\n", $output->fetch());
+
+    $logger->alert('Example');
+    self::assertEquals("[\e[31;1malert\e[39;22m] Example\n", $output->fetch());
+
+    $logger->critical('Example');
+    self::assertEquals("[\e[31;1mcritical\e[39;22m] Example\n", $output->fetch());
+
+    $logger->error('Example');
+    self::assertEquals("[\e[31;1merror\e[39;22m] Example\n", $output->fetch());
+
+    $logger->warning('Example');
+    self::assertEquals("[\e[33;1mwarning\e[39;22m] Example\n", $output->fetch());
+
+    $logger->notice('Example');
+    self::assertEquals('', $output->fetch());
+
+    $output->setVerbosity(OutputInterface::VERBOSITY_VERBOSE);
+    $logger->notice('Example');
+    self::assertEquals("[\e[32;1mnotice\e[39;22m] Example\n", $output->fetch());
+
+    $logger->info('Example');
+    self::assertEquals('', $output->fetch());
+
+    $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE);
+    $logger->info('Example');
+    self::assertEquals("[\e[32;1minfo\e[39;22m] Example\n", $output->fetch());
+
+    $logger->debug('Example');
+    self::assertEquals('', $output->fetch());
+
+    $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
+    $logger->debug('Example');
+    self::assertEquals("[\e[36;1mdebug\e[39;22m] Example\n", $output->fetch());
+
+    $logger->log(LogLevel::DEBUG, 'Foo: {foo}.', ['foo' => 'bar']);
+    self::assertEquals("[\e[36;1mdebug\e[39;22m] Foo: bar.\n", $output->fetch());
+
+    $this->expectException(InvalidArgumentException::class);
+    $logger->log('Unknown level', 'Example');
+  }
+
+}
