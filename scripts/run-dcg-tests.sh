8e1b420a6bb2c2495c55aa0085a7d2002dfd500a
Chi-teck <Chi-teck@users.noreply.github.com>
2019-05-13 21:24:55 +0500

    Format run-dcg-tests.sh script

--- scripts/run-dcg-tests.sh
+++ scripts/run-dcg-tests.sh
@@ -5,7 +5,9 @@ DCG_DIR=$(dirname $(readlink -f $0))'/..'
 
 echo -e "\e[104m ◂ PHPUnit ▸ \e[0m"
 $DCG_DIR/vendor/bin/phpunit -c $DCG_DIR
+
 echo -e "\n\e[104m ◂ Code sniffer ▸ \e[0m"
 $DCG_DIR/vendor/bin/phpcs -p --standard=$DCG_DIR/phpcs.xml
+
 echo -e "\e[104m ◂ Twig linter ▸ \e[0m"
 $DCG_DIR/vendor/bin/twigcs $DCG_DIR/templates
