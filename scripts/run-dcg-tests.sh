4190c0e2dd3ab0f71312822630d6b5f442b538b2
Chi-teck <Chi-teck@users.noreply.github.com>
2019-05-14 15:36:40 +0500

    Fix paths in run-dcg-test.sh script

--- scripts/run-dcg-tests.sh
+++ scripts/run-dcg-tests.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-DCG_DIR=$(dirname $(readlink -f $0))'/..'
+
+cd $(dirname $(readlink -f $0))'/..'
 
 echo -e "\e[104m ◂ PHPUnit ▸ \e[0m"
-$DCG_DIR/vendor/bin/phpunit -c $DCG_DIR
+./vendor/bin/phpunit
 
 echo -e "\n\e[104m ◂ Code sniffer ▸ \e[0m"
-$DCG_DIR/vendor/bin/phpcs -p --standard=$DCG_DIR/phpcs.xml
+./vendor/bin/phpcs -p
 
 echo -e "\e[104m ◂ Twig linter ▸ \e[0m"
-$DCG_DIR/vendor/bin/twigcs $DCG_DIR/templates
+./vendor/bin/twigcs ./templates
