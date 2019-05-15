ae75a82ee7d6d10156b998d0387b0dfef785b4a4
Chi-teck <Chi-teck@users.noreply.github.com>
2019-05-15 21:55:46 +0500

    Fix PHPStan output

--- scripts/run-dcg-tests.sh
+++ scripts/run-dcg-tests.sh
@@ -11,7 +11,7 @@ echo -e "\n\e[104m ◂ Code sniffer ▸ \e[0m"
 ./vendor/bin/phpcs -p
 
 echo -e "\n\e[104m ◂ PHP Static Analysis ▸ \e[0m"
-./vendor/bin/phpstan analyze
+./vendor/bin/phpstan analyze --ansi 2> /dev/null
 
 echo -e "\e[104m ◂ Twig linter ▸ \e[0m"
 ./vendor/bin/twigcs ./templates
