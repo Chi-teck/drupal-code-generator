d5720ec66361c83c03481727ec04e7673a72f658
Chi-teck <Chi-teck@users.noreply.github.com>
2019-05-16 13:21:46 +0500

    Clean up runt-dcg-tests.sh

--- scripts/run-dcg-tests.sh
+++ scripts/run-dcg-tests.sh
@@ -10,8 +10,5 @@ echo -e "\e[104m ◂ PHPUnit ▸ \e[0m"
 echo -e "\n\e[104m ◂ Code sniffer ▸ \e[0m"
 ./vendor/bin/phpcs -p
 
-echo -e "\n\e[104m ◂ PHP Static Analysis ▸ \e[0m"
-./vendor/bin/phpstan analyze --ansi 2> /dev/null
-
 echo -e "\e[104m ◂ Twig linter ▸ \e[0m"
 ./vendor/bin/twigcs ./templates
