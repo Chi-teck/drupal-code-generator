4829120240d1792a5ef82f81e1d8a216326a1bf2
Chi-teck <Chi-teck@users.noreply.github.com>
2019-05-28 22:16:01 +0500

    Allow appending yml files in configuration entity generator

--- src/Command/ConfigurationEntity.php
+++ src/Command/ConfigurationEntity.php
@@ -2,6 +2,8 @@
 
 namespace DrupalCodeGenerator\Command;
 
+use DrupalCodeGenerator\Asset;
+
 /**
  * Implements configuration-entity command.
  */
@@ -9,7 +11,7 @@ class ConfigurationEntity extends ModuleGenerator {
 
   protected $name = 'configuration-entity';
   protected $description = 'Generates configuration entity module';
-  protected $alias = 'configuration-entity';
+  protected $alias = 'config-entity';
 
   /**
    * {@inheritdoc}
@@ -21,24 +23,20 @@ class ConfigurationEntity extends ModuleGenerator {
     $vars['entity_type_id'] = $this->ask('Entity type ID', '{entity_type_label|h2m}');
     $vars['class_prefix'] = '{entity_type_id|camelize}';
 
-    $files = [
-      'src/ExampleListBuilder.php',
-      'src/Form/ExampleForm.php',
-      'src/ExampleInterface.php',
-      'src/Entity/Example.php',
-      'model.routing.yml',
-      'model.links.action.yml',
-      'model.links.menu.yml',
-      'model.permissions.yml',
-      'config/schema/model.schema.yml',
-    ];
-
-    $path_placeholders = ['model', 'Example'];
-    $path_replacements = [$vars['machine_name'], $vars['class_prefix']];
-    foreach ($files as $file) {
-      $this->addFile(str_replace($path_placeholders, $path_replacements, $file))
-        ->template('_configuration-entity/' . $file);
-    }
+    $this->addFile('src/{class_prefix}ListBuilder.php', '_configuration-entity/src/ExampleListBuilder.php');
+    $this->addFile('src/Form/{class_prefix}Form.php', '_configuration-entity/src/Form/ExampleForm.php');
+    $this->addFile('src/{class_prefix}Interface.php', '_configuration-entity/src/ExampleInterface.php');
+    $this->addFile('src/Entity/{class_prefix}.php', '_configuration-entity/src/Entity/Example.php');
+    $this->addFile('{machine_name}.routing.yml', '_configuration-entity/model.routing.yml')
+      ->action(Asset::APPEND);
+    $this->addFile('{machine_name}.links.action.yml', '_configuration-entity/model.links.action.yml')
+      ->action(Asset::APPEND);
+    $this->addFile('{machine_name}.links.menu.yml', '_configuration-entity/model.links.menu.yml')
+      ->action(Asset::APPEND);
+    $this->addFile('{machine_name}.permissions.yml', '_configuration-entity/model.permissions.yml')
+      ->action(Asset::APPEND);
+    $this->addFile('config/schema/{machine_name}.schema.yml', '_configuration-entity/config/schema/model.schema.yml')
+      ->action(Asset::APPEND);
 
     // Add 'configure' link to the info file if it exists.
     $update_info = function (?string $existing_content) use ($vars) {
