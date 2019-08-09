5882c6c5400b5969660f0fdf224f1ed640736dce
Chi-teck <Chi-teck@users.noreply.github.com>
2019-08-09 17:09:26 +0500

    Add SUT test for drupal context

---
+++ tests/sut/plazma/plazma.api.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Foo module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Provides list of bars.
+ */
+function hook_foo_bar() {
+  return [123];
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
