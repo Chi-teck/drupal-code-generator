e1dfb335319386c51419b0edc6348471cee8be66
Chi-teck <Chi-teck@users.noreply.github.com>
2020-02-19 03:37:07 +0000

    Clean-up dumper test

--- tests/dcg/Helper/DumperTest.php
+++ tests/dcg/Helper/DumperTest.php
@@ -25,21 +25,21 @@ final class DumperTest extends BaseTestCase {
    *
    * @var \Symfony\Component\Console\Input\ArrayInput
    */
-  protected $input;
+  private $input;
 
   /**
    * Console output.
    *
    * @var \Symfony\Component\Console\Output\BufferedOutput
    */
-  protected $output;
+  private $output;
 
   /**
    * The file system component.
    *
    * @var \Symfony\Component\Filesystem\Filesystem
    */
-  protected $filesystem;
+  private $filesystem;
 
   /**
    * {@inheritdoc}
@@ -54,211 +54,343 @@ final class DumperTest extends BaseTestCase {
   /**
    * Test callback.
    */
-  public function testDumper(): void {
-
-    // -- Default case.
+  public function testWithoutExistingFiles(): void {
     $assets = new AssetCollection();
     $assets[] = (new File('alpha.txt'))->content('alpha');
     $assets[] = (new File('beta.txt'))->content('beta');
     $assets[] = (new File('gamma.txt'))->content('gamma');
+
     $dumped_assets = $this->dump($assets);
-
     self::assertEquals($assets, $dumped_assets);
-    $this->assertFiles($dumped_assets);
-    self::assertOutput('');
 
-    // -- File exists and user confirms replacing (default action).
-    $this->filesystem->dumpFile($this->directory . '/foo.txt', 'old foo');
+    $expected_content = [
+      'alpha.txt' => 'alpha',
+      'beta.txt' => 'beta',
+      'gamma.txt' => 'gamma',
+    ];
+    $this->assertContent($expected_content);
+
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testUserImplicitlyConfirmsReplacing(): void {
     $this->createFile('foo.txt');
+
     $assets = new AssetCollection();
     $assets[] = (new File('foo.txt'))->content('foo');
-    $this->setStream("\n");
 
+    $this->setStream("\n");
     $dumped_assets = $this->dump($assets);
     self::assertEquals($assets, $dumped_assets);
-    $this->assertFiles($dumped_assets);
+
+    $expected_content = [
+      'foo.txt' => 'foo',
+    ];
+    $this->assertContent($expected_content);
+
     $expected_output = "\n";
     $expected_output .= " The file {dir}/foo.txt already exists. Would you like to replace it? [Yes]:\n";
-    $expected_output .= " ➤ ";
+    $expected_output .= ' ➤ ';
     $expected_output = str_replace('{dir}', $this->directory, $expected_output);
-    self::assertOutput($expected_output);
+    $this->assertOutput($expected_output);
+  }
 
-    // -- File exists and user confirms replacing.
+  /**
+   * Test callback.
+   */
+  public function testUserConfirmsReplacing(): void {
     $this->createFile('bar.txt');
+
     $assets = new AssetCollection();
     $assets[] = (new File('bar.txt'))->content('bar');
+
     $this->setStream("Yes\n");
     $dumped_assets = $this->dump($assets);
-
     self::assertEquals($assets, $dumped_assets);
-    $this->assertFiles($dumped_assets);
+
+    $expected_content = [
+      'bar.txt' => 'bar',
+    ];
+    $this->assertContent($expected_content);
+
     $expected_output = "\n";
     $expected_output .= " The file {dir}/bar.txt already exists. Would you like to replace it? [Yes]:\n";
-    $expected_output .= " ➤ ";
-    self::assertOutput($expected_output);
+    $expected_output .= ' ➤ ';
+    $this->assertOutput($expected_output);
+  }
 
-    // -- File exists and user cancels replacing.
-    $this->createFile('example.txt');
+  /**
+   * Test callback.
+   */
+  public function testUserCancelsReplacing(): void {
+    $this->createFile('example.txt', 'Example');
+
     $assets = new AssetCollection();
     $assets[] = (new File('example.txt'))->content('example');
+
     $this->setStream("No\n");
     $dumped_assets = $this->dump($assets);
-
     self::assertEquals(new AssetCollection(), $dumped_assets);
-    $this->assertFiles($dumped_assets);
+
+    $expected_content = [
+      'example.txt' => 'Example',
+    ];
+    $this->assertContent($expected_content);
+
     $expected_output = "\n";
     $expected_output .= " The file {dir}/example.txt already exists. Would you like to replace it? [Yes]:\n";
-    $expected_output .= " ➤ ";
-    self::assertOutput($expected_output);
+    $expected_output .= ' ➤ ';
+    $this->assertOutput($expected_output);
+  }
 
-    // -- Dumper with enabled replace option (always yes).
+  /**
+   * Test callback.
+   */
+  public function testReplaceOptionYes(): void {
     $this->createFile('wine.txt');
+
     $assets = new AssetCollection();
-    $assets[] = (new File('wine.txt'))->content('wine');
+    $assets[] = (new File('wine.txt'))->content('Wine');
+
     $dumped_assets = $this->dump($assets, TRUE);
-
     self::assertEquals($assets, $dumped_assets);
-    $this->assertFiles($dumped_assets);
-    self::assertOutput('');
 
-    // -- Dumper with enabled replace option (always no).
-    $this->createFile('beer.txt');
+    $expected_content = [
+      'wine.txt' => 'Wine',
+    ];
+    $this->assertContent($expected_content);
+
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testReplaceOptionNo(): void {
+    $this->createFile('beer.txt', 'Old beer');
+
     $assets = new AssetCollection();
-    $assets[] = (new File('beer.txt'))->content('beer');
+    $assets[] = (new File('beer.txt'))->content('New beer');
+
     $dumped_assets = $this->dump($assets, FALSE);
-
     self::assertEquals(new AssetCollection(), $dumped_assets);
-    $this->assertFiles($dumped_assets);
-    self::assertOutput('');
 
-    // -- File with special permissions.
+    $expected_content = [
+      'beer.txt' => 'Old beer',
+    ];
+    $this->assertContent($expected_content);
+
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testFilePermissions(): void {
     $assets = new AssetCollection();
-    $assets[] = (new File('prize.txt'))->content('prize')->mode(0757);
-    $dumped_assets = $this->dump($assets);
+    $assets[] = (new File('prize.txt'))->content('Prize')->mode(0757);
 
+    $dumped_assets = $this->dump($assets);
     self::assertEquals($assets, $dumped_assets);
-    $this->assertFiles($dumped_assets);
-    self::assertOutput('');
+
+    $expected_content = [
+      'prize.txt' => 'Prize',
+    ];
+    $this->assertContent($expected_content);
+
     $permissions = decoct(fileperms($this->directory . '/prize.txt'));
     self::assertEquals($permissions, '100757');
 
-    // -- Directory.
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testDirectory(): void {
     $assets = new AssetCollection();
     $assets[] = new Directory('includes');
+
     $dumped_assets = $this->dump($assets);
-
     self::assertEquals($assets, $dumped_assets);
-    self::assertDirectoryExists($this->directory . '/includes');
-    self::assertOutput('');
 
-    // -- Existing directory.
-    $this->filesystem->dumpFile($this->directory . '/core/readme.txt', 'old readme');
-    $assets = new AssetCollection();
-    $assets[] = new Directory('core');
-    $dumped_assets = $this->dump($assets);
+    $expected_content = [
+      'includes' => [],
+    ];
+    $this->assertContent($expected_content);
 
-    self::assertEquals(new AssetCollection(), $dumped_assets);
-    self::assertDirectoryExists($this->directory . '/core');
-    // Make sure that files in the directory have not been overwritten.
-    static::assertFileExists($this->directory . '/core/readme.txt');
-    self::assertOutput('');
+    $this->assertEmptyOutput();
+  }
 
-    // -- Prepend file content.
-    $this->filesystem->dumpFile($this->directory . '/log.txt', 'File header');
+  /**
+   * Test callback.
+   */
+  public function testPrependFile(): void {
+    $this->createFile('log.txt', 'Record 1');
 
     $assets = new AssetCollection();
-    $assets[] = (new File('log.txt'))->content('Record 1')->prependIfExists();
+    $assets[] = (new File('log.txt'))->content('File header')->prependIfExists();
+
     $dumped_assets = $this->dump($assets, TRUE);
-
     self::assertEquals($assets, $dumped_assets);
-    $content = "Record 1\nFile header";
-    self::assertStringEqualsFile($this->directory . '/log.txt', $content);
-    self::assertOutput('');
 
-    // -- Append file content.
-    $this->filesystem->dumpFile($this->directory . '/log.txt', 'File header');
+    $expected_content = [
+      'log.txt' => "File header\nRecord 1",
+    ];
+    $this->assertContent($expected_content);
+
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testAppendFile(): void {
+    $this->createFile('log.txt', 'File header');
 
     $assets = new AssetCollection();
-    $assets[] = (new File('log.txt'))->content("redundant line\nRecord 1")->appendIfExists()->headerSize(1);
-    $assets[] = (new File('log.txt'))->content('Record 2')->appendIfExists();
+    $assets[] = (new File('log.txt'))
+      ->content("redundant line\nRecord 1")
+      ->appendIfExists()
+      ->headerSize(1);
+    $assets[] = (new File('log.txt'))
+      ->content('Record 2')
+      ->appendIfExists();
+
     $dumped_assets = $this->dump($assets, TRUE);
-
     self::assertEquals($assets, $dumped_assets);
-    $content = "File header\nRecord 1\nRecord 2";
-    self::assertStringEqualsFile($this->directory . '/log.txt', $content);
-    self::assertOutput('');
 
-    // -- Skip file.
-    $this->filesystem->dumpFile($this->directory . '/log.txt', 'Existing record');
+    $expected_content = [
+      'log.txt' => "File header\nRecord 1\nRecord 2",
+    ];
+    $this->assertContent($expected_content);
+
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testSkipFile(): void {
+    $this->createFile('log.txt', 'Existing record');
 
     $assets = new AssetCollection();
     $assets[] = (new File('log.txt'))->content('New Record')->skipIfExists();
+
     $dumped_assets = $this->dump($assets, TRUE);
-
     self::assertEquals(new AssetCollection(), $dumped_assets);
-    self::assertStringEqualsFile($this->directory . '/log.txt', 'Existing record');
-    self::assertOutput('');
 
-    // -- Symlink
+    $expected_content = [
+      'log.txt' => 'Existing record',
+    ];
+    $this->assertContent($expected_content);
+
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testSymlink(): void {
     $assets = new AssetCollection();
     $assets[] = new Directory('Alpha/Beta/Gamma');
-    $assets[] = (new File('foo.txt'))->content('bar');
+    $assets[] = (new File('foo.txt'))->content('Bar');
     $assets[] = new Symlink('foo.link', 'foo.txt');
     $assets[] = new Symlink('abg.link', 'Alpha/Beta/Gamma');
+
     $dumped_assets = $this->dump($assets, TRUE);
     self::assertEquals($assets, $dumped_assets);
-    self::assertStringEqualsFile($this->directory . '/foo.link', 'bar');
-    self::assertDirectoryExists($this->directory . '/abg.link');
-    self::assertOutput('');
 
-    // -- Existing symlink.
+    $expected_content = [
+      'Alpha/Beta/Gamma' => [],
+      'Alpha/Beta' => [],
+      'Alpha' => [],
+      'abg.link (Alpha/Beta/Gamma)' => [],
+      'foo.link (foo.txt)' => 'Bar',
+      'foo.txt' => 'Bar',
+    ];
+    $this->assertContent($expected_content);
+
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testSymlinkAndExistingFile(): void {
+    $this->createFile('foo.link', 'Existing content');
+
     $assets = new AssetCollection();
-    $assets[] = (new File('bar.txt'))->content('foo');
     $assets[] = (new Symlink('foo.link', 'foo.txt'))->skipIfExists();
-    $dumped_assets = $this->dump($assets, TRUE);
-    unset($assets[1]);
-    self::assertEquals($assets, $dumped_assets);
-    self::assertStringEqualsFile($this->directory . '/foo.link', 'bar');
-    self::assertOutput('');
 
-    // -- Dry dump.
+    $dumped_assets = $this->dump($assets, TRUE);
+    self::assertEquals(new AssetCollection(), $dumped_assets);
+
+    $expected_content = [
+      'foo.link' => 'Existing content',
+    ];
+    $this->assertContent($expected_content);
+
+    $this->assertEmptyOutput();
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testDryDump(): void {
     $assets = new AssetCollection();
     $assets[] = (new File('example.txt'))->content('Example');
     $assets[] = new Directory('foo');
-    $dumped_assets = $this->dump($assets, NULL, TRUE);
+    $assets[] = new Symlink('foo.link', 'example.txt');
 
+    $dumped_assets = $this->dump($assets, NULL, TRUE);
     self::assertEquals(new AssetCollection(), $dumped_assets);
+
+    self::assertDirectoryNotExists($this->directory);
+
     $expected_output = "\n";
-    $expected_output .= " /tmp/dcg_sandbox/foo (empty directory)\n";
+    $expected_output .= " {$this->directory}/foo (empty directory)\n";
     $expected_output .= "––––––––––––––––––––––––––––––––––––––––\n";
     $expected_output .= "\n";
-    $expected_output .= " /tmp/dcg_sandbox/example.txt\n";
+    $expected_output .= " {$this->directory}/example.txt\n";
     $expected_output .= "––––––––––––––––––––––––––––––\n";
     $expected_output .= "Example\n";
-    self::assertOutput($expected_output);
+    $expected_output .= "\n";
+    $expected_output .= " {$this->directory}/foo.link\n";
+    $expected_output .= "–––––––––––––––––––––––––––\n";
+    $expected_output .= "Symlink to example.txt\n";
+    $this->assertOutput($expected_output);
   }
 
   /**
    * Asserts dumper output.
    */
-  protected function assertOutput(string $expected_output): void {
+  private function assertOutput(string $expected_output): void {
     $expected_output = str_replace('{dir}', $this->directory, $expected_output);
-    static::assertEquals($expected_output, $this->output->fetch());
+    self::assertEquals($expected_output, $this->output->fetch());
   }
 
   /**
-   * Asserts contents of dumped assets.
+   * Asserts empty dumper output.
    */
-  protected function assertFiles(AssetCollection $assets): void {
-    foreach ($assets as $asset) {
-      static::assertStringEqualsFile($this->directory . '/' . $asset->getPath(), $asset->getContent());
-    }
+  private function assertEmptyOutput(): void {
+    self::assertOutput('');
+  }
+
+  /**
+   * Asserts content.
+   */
+  private function assertContent(array $content): void {
+    self::assertSame($content, $this->readAssets($this->directory));
   }
 
   /**
    * Dumps assets into file system.
    */
-  protected function dump(AssetCollection $assets, ?bool $replace = NULL, bool $dry_run = FALSE): AssetCollection {
+  private function dump(AssetCollection $assets, ?bool $replace = NULL, bool $dry_run = FALSE): AssetCollection {
     $question_helper = new QuestionHelper();
     $helper_set = new HelperSet();
     $helper_set->set(new QuestionHelper());
@@ -275,7 +407,7 @@ final class DumperTest extends BaseTestCase {
    * @param string $input
    *   Input that is to be written.
    */
-  protected function setStream(string $input): void {
+  private function setStream(string $input): void {
     $stream = fopen('php://memory', 'r+', FALSE);
     fwrite($stream, $input);
     rewind($stream);
@@ -285,8 +417,32 @@ final class DumperTest extends BaseTestCase {
   /**
    * Creates a file.
    */
-  protected function createFile(string $file_name): void {
-    $this->filesystem->dumpFile($this->directory . '/' . $file_name, mt_rand());
+  private function createFile(string $file_name, string $content = ''): void {
+    $this->filesystem->dumpFile($this->directory . '/' . $file_name, $content);
+  }
+
+  /**
+   * Recursively read assets.
+   */
+  private function readAssets(string $directory): array {
+    $results = [];
+    foreach (scandir($directory) as $file) {
+      if ($file != '.' && $file != '..') {
+        $path = $directory . DIRECTORY_SEPARATOR . $file;
+        $relative_path = rtrim($this->filesystem->makePathRelative($path, $this->directory), '/');
+        if (is_link($path)) {
+          $relative_path .= ' (' . readlink($path) . ')';
+        }
+        if (is_dir($path)) {
+          $results += self::readAssets($path);
+          $results[$relative_path] = [];
+        }
+        else {
+          $results[$relative_path] = file_get_contents($path);
+        }
+      }
+    }
+    return $results;
   }
 
 }
