f3dd0ab6569d0c45feae6f7a03555f8b991fc54f
Chi-teck <Chi-teck@users.noreply.github.com>
2020-02-20 08:49:20 +0000

    Add generate-completion command

---
+++ src/Command/GenerateCompletion.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DrupalCodeGenerator\Command;
+
+use DrupalCodeGenerator\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Implements generate-completion command.
+ */
+class GenerateCompletion extends Command {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('generate-completion')
+      ->setDescription('Generates shell completion')
+      ->addUsage('--shell=bash >> ~/.bash_completion')
+      ->addOption('shell', NULL, InputOption::VALUE_OPTIONAL, 'Shell type', 'bash');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $shell = $input->getOption('shell');
+    if (!in_array($shell, ['bash', 'fish', 'zsh'])) {
+      $message = sprintf('<error>%s shell is not supported.</error>', strip_tags($shell));
+      /** @var \Symfony\Component\Console\Output\ConsoleOutput $output */
+      $output->getErrorOutput()->writeLn($message);
+      return 1;
+    }
+    $content = file_get_contents(Application::ROOT . "/resources/$shell-completion");
+    $output->writeln($content, OutputInterface::OUTPUT_RAW);
+    return 0;
+  }
+
+}
