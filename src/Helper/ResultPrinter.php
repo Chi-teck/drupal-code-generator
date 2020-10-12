2106965791019696df9c076a46c35909c84cccee
Chi-teck <Chi-teck@users.noreply.github.com>
2020-10-12 04:53:18 +0000

    Clean-up result printer

--- src/Helper/ResultPrinter.php
+++ src/Helper/ResultPrinter.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Helper\TableStyle;
 
 /**
- * Output printer for generators.
+ * Result printer for generators.
  */
 class ResultPrinter extends Helper implements IOAwareInterface {
 
@@ -33,75 +33,77 @@ class ResultPrinter extends Helper implements IOAwareInterface {
    *   (Optional) Base path.
    */
   public function printResult(AssetCollection $assets, string $base_path = ''): void {
-    if (\count($assets) == 0) {
-      return;
+    if (\count($assets) > 0) {
+      $this->io->title('The following directories and files have been created or updated:');
+      $this->io->isVerbose() ?
+        $this->printTable($assets, $base_path) : $this->printList($assets, $base_path);
+    }
+  }
+
+  /**
+   * Prints assets in tabular form.
+   */
+  protected function printTable(AssetCollection $assets, string $base_path): void {
+
+    $headers[] = ['Type', 'Path', 'Lines', 'Size'];
+
+    $rows = [];
+
+    foreach ($assets->getDirectories()->getSorted() as $directory) {
+      $rows[] = ['directory', $this->formatPath($base_path, $directory), '-', '-'];
     }
 
-    $this->io->title('The following directories and files have been created or updated:');
-
-    // -- Table.
-    if ($this->io->isVerbose()) {
-      $headers[] = ['Type', 'Path', 'Lines', 'Size'];
-
-      $rows = [];
-
-      foreach ($assets->getDirectories()->getSorted() as $directory) {
-        // phpcs:ignore Drupal.Arrays.Array.LongLineDeclaration
-        $rows[] = ['directory', $this->formatPath($base_path, $directory), '-', '-'];
-      }
-
-      $total_size = 0;
-      $total_lines = 0;
-
-      foreach ($assets->getFiles()->getSorted() as $file) {
-        $size = \mb_strlen($file->getContent());
-        $total_size += $size;
-        $lines = $size == 0 ? 0 : \substr_count($file->getContent(), "\n") + 1;
-        $total_lines += $lines;
-        $rows[] = ['file', $this->formatPath($base_path, $file), $lines, $size];
-      }
-
-      foreach ($assets->getSymlinks()->getSorted() as $symlink) {
-        $rows[] = ['symlink', $this->formatPath($base_path, $symlink), '-', '-'];
-      }
-
-      $rows[] = new TableSeparator();
-
-      // Summary.
-      $total_assets = \count($assets);
-      $rows[] = [
-        NULL,
-        \sprintf('Total: %d %s', $total_assets, $total_assets == 1 ? 'asset' : 'assets'),
-        $total_lines,
-        self::formatMemory($total_size),
-      ];
-
-      $right_aligned = (new TableStyle())->setPadType(\STR_PAD_LEFT);
-      $this->io
-        ->buildTable($headers, $rows)
-        ->setColumnStyle(2, $right_aligned)
-        ->setColumnStyle(3, $right_aligned)
-        ->render();
-
-      $this->io->newLine();
-    }
-    // -- Bulleted list.
-    else {
-      $dumped_files = [];
-      // Group results by asset type.
-      $assets = $assets->getSorted();
-      foreach ($assets->getDirectories() as $directory) {
-        $dumped_files[] = $this->formatPath($base_path, $directory);
-      }
-      foreach ($assets->getFiles() as $file) {
-        $dumped_files[] = $this->formatPath($base_path, $file);
-      }
-      foreach ($assets->getSymlinks() as $symlink) {
-        $dumped_files[] = $this->formatPath($base_path, $symlink);
-      }
-      $this->io->listing($dumped_files);
+    $total_size = $total_lines = 0;
+    foreach ($assets->getFiles()->getSorted() as $file) {
+      $size = \mb_strlen($file->getContent());
+      $total_size += $size;
+      $lines = $size == 0 ? 0 : \substr_count($file->getContent(), "\n") + 1;
+      $total_lines += $lines;
+      $rows[] = ['file', $this->formatPath($base_path, $file), $lines, $size];
     }
 
+    foreach ($assets->getSymlinks()->getSorted() as $symlink) {
+      $rows[] = ['symlink', $this->formatPath($base_path, $symlink), '-', '-'];
+    }
+
+    $rows[] = new TableSeparator();
+
+    // Summary.
+    $total_assets = \count($assets);
+    $rows[] = [
+      NULL,
+      \sprintf('Total: %d %s', $total_assets, $total_assets == 1 ? 'asset' : 'assets'),
+      $total_lines,
+      self::formatMemory($total_size),
+    ];
+
+    $right_aligned = (new TableStyle())->setPadType(\STR_PAD_LEFT);
+    $this->io
+      ->buildTable($headers, $rows)
+      ->setColumnStyle(2, $right_aligned)
+      ->setColumnStyle(3, $right_aligned)
+      ->render();
+
+    $this->io->newLine();
+  }
+
+  /**
+   * Prints assets as a bulleted list.
+   */
+  protected function printList(AssetCollection $assets, string $base_path): void {
+    $dumped_files = [];
+    // Group results by asset type.
+    $assets = $assets->getSorted();
+    foreach ($assets->getDirectories() as $directory) {
+      $dumped_files[] = $this->formatPath($base_path, $directory);
+    }
+    foreach ($assets->getFiles() as $file) {
+      $dumped_files[] = $this->formatPath($base_path, $file);
+    }
+    foreach ($assets->getSymlinks() as $symlink) {
+      $dumped_files[] = $this->formatPath($base_path, $symlink);
+    }
+    $this->io->listing($dumped_files);
   }
 
   /**
