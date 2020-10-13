6e87e1f933a8f95a69a9dfc1184db1ceaa7ea413
Chi-teck <Chi-teck@users.noreply.github.com>
2020-10-13 08:06:10 +0000

    Clean-up renderer test

--- tests/dcg/Helper/RendererTest.php
+++ tests/dcg/Helper/RendererTest.php
@@ -16,49 +16,81 @@ use Twig\Loader\FilesystemLoader;
 final class RendererTest extends TestCase {
 
   /**
-   * Test callback.
+   * The renderer to test.
+   *
+   * @var \DrupalCodeGenerator\Helper\Renderer
    */
-  public function testRenderer(): void {
+  private $renderer;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
 
     $twig_loader = new FilesystemLoader();
     $twig = new TwigEnvironment($twig_loader);
-    $renderer = new Renderer($twig);
-    $renderer->addPath(__DIR__);
+    $this->renderer = new Renderer($twig);
+    $this->renderer->addPath(__DIR__);
     $logger = new ConsoleLogger(new NullOutput());
-    $renderer->setLogger($logger);
+    $this->renderer->setLogger($logger);
+  }
 
-    self::assertSame($renderer->getName(), 'renderer');
-
-    $content = $renderer->render('_template.twig', ['value' => 'example']);
+  /**
+   * Test callback.
+   */
+  public function testRender(): void {
+    $content = $this->renderer->render('_template.twig', ['value' => 'example']);
     self::assertSame($content, "The value is example.\n");
+  }
 
+  /**
+   * Test callback.
+   */
+  public function testRenderAsset(): void {
     $asset = (new File('foo'))
       ->template('_template.twig')
       ->vars(['value' => 'foo']);
-    $renderer->renderAsset($asset);
+    $this->renderer->renderAsset($asset);
     self::assertSame("The value is foo.\n", $asset->getContent());
-    $asset->vars(['value' => 'bar']);
-    $renderer->renderAsset($asset);
-    self::assertSame("The value is bar.\n", $asset->getContent());
 
+    $asset->vars(['value' => 'bar']);
+    $this->renderer->renderAsset($asset);
+    self::assertSame("The value is bar.\n", $asset->getContent());
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testRenderAssetWithHeaderTemplate(): void {
     $asset = (new File('foo'))
       ->template('_template.twig')
       ->vars(['name' => 'foo', 'value' => 'bar'])
       ->headerTemplate('_header_template.twig');
-    $renderer->renderAsset($asset);
+    $this->renderer->renderAsset($asset);
     $expected_content = "The name is foo.\n\nThe value is bar.\n";
     self::assertSame($expected_content, $asset->getContent());
+  }
 
+  /**
+   * Test callback.
+   */
+  public function testRenderAssetWithContent(): void {
     $asset = (new File('foo'))
       ->content('example')
       ->template(NULL);
-    $renderer->renderAsset($asset);
+    $this->renderer->renderAsset($asset);
     self::assertSame('example', $asset->getContent());
+  }
 
+  /**
+   * Test callback.
+   */
+  public function testRenderAssetWithInlineTemplate(): void {
     $asset = (new File('foo'))
       ->inlineTemplate('{{ a }} + {{ b }} = {{ a + b }}')
       ->vars(['a' => '2', 'b' => '3']);
-    $renderer->renderAsset($asset);
+    $this->renderer->renderAsset($asset);
     self::assertSame('2 + 3 = 5', $asset->getContent());
   }
 
