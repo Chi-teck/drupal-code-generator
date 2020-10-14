b33b7ac71f8a4de227eec82fc7d6939d525d07a8
Chi-teck <Chi-teck@users.noreply.github.com>
2020-10-14 07:33:10 +0000

    Update theme-settings test

--- tests/dcg/Generator/ThemeSettingsTest.php
+++ tests/dcg/Generator/ThemeSettingsTest.php
@@ -2,22 +2,47 @@
 
 namespace DrupalCodeGenerator\Tests\Generator;
 
+use DrupalCodeGenerator\Command\ThemeSettings;
+use DrupalCodeGenerator\Test\GeneratorTest;
+
 /**
  * Test for theme-settings command.
  */
-final class ThemeSettingsTest extends BaseGeneratorTest {
+final class ThemeSettingsTest extends GeneratorTest {
 
-  protected $class = 'ThemeSettings';
+  protected $fixtureDir = __DIR__;
 
-  protected $interaction = [
-    'Theme name [%default_name%]:' => 'Foo',
-    'Theme machine name [foo]:' => 'foo',
-  ];
+  /**
+   * Test callback.
+   */
+  public function testGenerator(): void {
 
-  protected $fixtures = [
-    'theme-settings.php' => '/_theme_settings_form.php',
-    'config/install/foo.settings.yml' => '/_theme_settings_config.yml',
-    'config/schema/foo.schema.yml' => '/_theme_settings_schema.yml',
-  ];
+    $this->execute(new ThemeSettings(), ['Foo', 'foo']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to theme-settings generator!
+    ––––––––––––––––––––––––––––––––––––––
+
+     Theme name [%default_name%]:
+     ➤ 
+
+     Theme machine name [foo]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • theme-settings.php
+     • config/install/foo.settings.yml
+     • config/schema/foo.schema.yml
+
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('theme-settings.php', '_theme_settings_form.php');
+    $this->assertGeneratedFile('config/install/foo.settings.yml', '_theme_settings_config.yml');
+    $this->assertGeneratedFile('config/schema/foo.schema.yml', '_theme_settings_schema.yml');
+  }
 
 }
