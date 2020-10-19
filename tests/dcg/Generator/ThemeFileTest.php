714853e709bdaf1855aa87bc424788c7bf6451e1
Chi-teck <Chi-teck@users.noreply.github.com>
2020-10-19 06:07:08 +0000

    Update ThemeFileTest

--- tests/dcg/Generator/ThemeFileTest.php
+++ tests/dcg/Generator/ThemeFileTest.php
@@ -2,20 +2,43 @@
 
 namespace DrupalCodeGenerator\Tests\Generator;
 
+use DrupalCodeGenerator\Command\ThemeFile;
+use DrupalCodeGenerator\Test\GeneratorTest;
+
 /**
  * Test for theme-file command.
  */
-final class ThemeFileTest extends BaseGeneratorTest {
+final class ThemeFileTest extends GeneratorTest {
 
-  protected $class = 'ThemeFile';
+  protected $fixtureDir = __DIR__;
 
-  protected $interaction = [
-    'Theme name [%default_name%]:' => 'Foo',
-    'Theme machine name [foo]:' => 'foo',
-  ];
+  /**
+   * Test callback.
+   */
+  public function testGenerator(): void {
 
-  protected $fixtures = [
-    'foo.theme' => '/_.theme',
-  ];
+    $this->execute(new ThemeFile(), ['Foo', 'foo']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to theme-file generator!
+    ––––––––––––––––––––––––––––––––––
+
+     Theme name [%default_name%]:
+     ➤ 
+
+     Theme machine name [foo]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • foo.theme
+
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('foo.theme', '_.theme');
+  }
 
 }
