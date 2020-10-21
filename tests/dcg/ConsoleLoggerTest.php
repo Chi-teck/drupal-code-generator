31a0dbc633f407117222431f6668c355689e3246
Chi-teck <Chi-teck@users.noreply.github.com>
2020-10-21 08:36:19 +0000

    Clean-up Logger test

--- tests/dcg/ConsoleLoggerTest.php
+++ tests/dcg/ConsoleLoggerTest.php
@@ -5,9 +5,9 @@ namespace DrupalCodeGenerator\Tests;
 use DrupalCodeGenerator\Logger\ConsoleLogger;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\InvalidArgumentException;
-use Psr\Log\LogLevel;
+use Psr\Log\LogLevel as LL;
 use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\OutputInterface as OI;
 
 /**
  * A test for console logger.
@@ -16,53 +16,208 @@ final class ConsoleLoggerTest extends TestCase {
 
   /**
    * Test callback.
+   *
+   * @dataProvider outputMappingProvider()
    */
-  public function testLogger(): void {
+  public function testOutputMapping(string $log_level, int $verbosity, string $expected_output): void {
+    $output = new BufferedOutput($verbosity, TRUE);
+    $logger = new ConsoleLogger($output);
+    $logger->log($log_level, 'Example');
+    self::assertSame($expected_output, $output->fetch());
+  }
 
-    $output = new BufferedOutput(OutputInterface::VERBOSITY_NORMAL, TRUE);
+  /**
+   * Data provider for testOutputMapping().
+   */
+  public static function outputMappingProvider(): array {
+    $mapping = [
+      [
+        LL::EMERGENCY,
+        OI::VERBOSITY_NORMAL,
+        '[<fg=red;options=bold;>emergency</>] Example',
+      ],
+      [
+        LL::ALERT,
+        OI::VERBOSITY_NORMAL,
+        '[<fg=red;options=bold;>alert</>] Example',
+      ],
+      [
+        LL::CRITICAL,
+        OI::VERBOSITY_NORMAL,
+        '[<fg=red;options=bold;>critical</>] Example',
+      ],
+      [
+        LL::WARNING,
+        OI::VERBOSITY_NORMAL,
+        '[<fg=yellow;options=bold;>warning</>] Example',
+      ],
+      [LL::NOTICE, OI::VERBOSITY_NORMAL, ''],
+      [LL::INFO, OI::VERBOSITY_NORMAL, ''],
+      [LL::DEBUG, OI::VERBOSITY_NORMAL, ''],
+
+      [
+        LL::EMERGENCY,
+        OI::VERBOSITY_VERBOSE,
+        '[<fg=red;options=bold;>emergency</>] Example',
+      ],
+      [
+        LL::ALERT,
+        OI::VERBOSITY_VERBOSE,
+        '[<fg=red;options=bold;>alert</>] Example',
+      ],
+      [
+        LL::CRITICAL,
+        OI::VERBOSITY_VERBOSE,
+        '[<fg=red;options=bold;>critical</>] Example',
+      ],
+      [
+        LL::WARNING,
+        OI::VERBOSITY_VERBOSE,
+        '[<fg=yellow;options=bold;>warning</>] Example',
+      ],
+      [
+        LL::NOTICE,
+        OI::VERBOSITY_VERBOSE,
+        '[<fg=green;options=bold;>notice</>] Example',
+      ],
+      [LL::INFO, OI::VERBOSITY_VERBOSE, ''],
+      [LL::DEBUG, OI::VERBOSITY_VERBOSE, ''],
+
+      [
+        LL::EMERGENCY,
+        OI::VERBOSITY_VERY_VERBOSE,
+        '[<fg=red;options=bold;>emergency</>] Example',
+      ],
+      [
+        LL::ALERT,
+        OI::VERBOSITY_VERY_VERBOSE,
+        '[<fg=red;options=bold;>alert</>] Example',
+      ],
+      [
+        LL::CRITICAL,
+        OI::VERBOSITY_VERY_VERBOSE,
+        '[<fg=red;options=bold;>critical</>] Example',
+      ],
+      [
+        LL::WARNING,
+        OI::VERBOSITY_VERY_VERBOSE,
+        '[<fg=yellow;options=bold;>warning</>] Example',
+      ],
+      [
+        LL::NOTICE,
+        OI::VERBOSITY_VERY_VERBOSE,
+        '[<fg=green;options=bold;>notice</>] Example',
+      ],
+      [
+        LL::INFO,
+        OI::VERBOSITY_VERY_VERBOSE,
+        '[<fg=green;options=bold;>info</>] Example',
+      ],
+      [LL::DEBUG, OI::VERBOSITY_VERY_VERBOSE, ''],
+
+      [
+        LL::EMERGENCY,
+        OI::VERBOSITY_DEBUG,
+        '[<fg=red;options=bold;>emergency</>] Example',
+      ],
+      [
+        LL::ALERT,
+        OI::VERBOSITY_DEBUG,
+        '[<fg=red;options=bold;>alert</>] Example',
+      ],
+      [
+        LL::CRITICAL,
+        OI::VERBOSITY_DEBUG,
+        '[<fg=red;options=bold;>critical</>] Example',
+      ],
+      [
+        LL::WARNING,
+        OI::VERBOSITY_DEBUG,
+        '[<fg=yellow;options=bold;>warning</>] Example',
+      ],
+      [
+        LL::NOTICE,
+        OI::VERBOSITY_DEBUG,
+        '[<fg=green;options=bold;>notice</>] Example',
+      ],
+      [
+        LL::INFO,
+        OI::VERBOSITY_DEBUG,
+        '[<fg=green;options=bold;>info</>] Example',
+      ],
+      [
+        LL::DEBUG,
+        OI::VERBOSITY_DEBUG,
+        '[<fg=cyan;options=bold;>debug</>] Example',
+      ],
+    ];
+
+    // Render expected output.
+    \array_walk(
+      $mapping,
+      static function (&$row): void {
+        if ($row[2]) {
+          $output = new BufferedOutput($row[1], TRUE);
+          $output->writeln($row[2]);
+          $row[2] = $output->fetch();
+        }
+      },
+    );
+
+    return $mapping;
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testThrowsOnWrongLogLevel(): void {
+    $logger = new ConsoleLogger(new BufferedOutput(OI::VERBOSITY_NORMAL, TRUE));
+    $this->expectException(InvalidArgumentException::class);
+    $this->expectExceptionMessage('The log level "Unknown level" does not exist.');
+    $logger->log('Unknown level', 'Example');
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testInterpolation(): void {
+    $output = new BufferedOutput(OI::VERBOSITY_NORMAL, FALSE);
     $logger = new ConsoleLogger($output);
 
-    $logger->emergency('Example');
-    self::assertSame("[\e[31;1memergency\e[39;22m] Example\n", $output->fetch());
+    $message = <<< 'EOT'
 
-    $logger->alert('Example');
-    self::assertSame("[\e[31;1malert\e[39;22m] Example\n", $output->fetch());
+            Value 1: {v1}
+            Value 2: {v2}
+            Value 3: {v3}
+            Value 4: {v4}
+            Value 5: {v5}
+        EOT;
+    $context = [
+      'v1' => 'Example',
+      'v2' => new \DateTime('2000-01-01T00:00:00+00:00'),
+      'v3' => new \stdClass(),
 
-    $logger->critical('Example');
-    self::assertSame("[\e[31;1mcritical\e[39;22m] Example\n", $output->fetch());
+      'v4' => new class {
+        // phpcs:ignore Drupal.Commenting.FunctionComment.Missing
+        public function __toString(): string {
+          return 'This is foo object.';
+        }
 
-    $logger->error('Example');
-    self::assertSame("[\e[31;1merror\e[39;22m] Example\n", $output->fetch());
+      },
+      'v5' => [],
+    ];
+    $logger->log(LL::WARNING, $message, $context);
 
-    $logger->warning('Example');
-    self::assertSame("[\e[33;1mwarning\e[39;22m] Example\n", $output->fetch());
+    $expected_output = <<< 'EOT'
+        [warning] 
+            Value 1: Example
+            Value 2: 2000-01-01T00:00:00+00:00
+            Value 3: [object stdClass]
+            Value 4: This is foo object.
+            Value 5: [array]
 
-    $logger->notice('Example');
-    self::assertSame('', $output->fetch());
-
-    $output->setVerbosity(OutputInterface::VERBOSITY_VERBOSE);
-    $logger->notice('Example');
-    self::assertSame("[\e[32;1mnotice\e[39;22m] Example\n", $output->fetch());
-
-    $logger->info('Example');
-    self::assertSame('', $output->fetch());
-
-    $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE);
-    $logger->info('Example');
-    self::assertSame("[\e[32;1minfo\e[39;22m] Example\n", $output->fetch());
-
-    $logger->debug('Example');
-    self::assertSame('', $output->fetch());
-
-    $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
-    $logger->debug('Example');
-    self::assertSame("[\e[36;1mdebug\e[39;22m] Example\n", $output->fetch());
-
-    $logger->log(LogLevel::DEBUG, 'Foo: {foo}.', ['foo' => 'bar']);
-    self::assertSame("[\e[36;1mdebug\e[39;22m] Foo: bar.\n", $output->fetch());
-
-    $this->expectException(InvalidArgumentException::class);
-    $logger->log('Unknown level', 'Example');
+        EOT;
+    self::assertSame($expected_output, $output->fetch());
   }
 
 }
