6b3ed0f98384e4317b293b146f8b524ef9dd01da
Chi-teck <Chi-teck@users.noreply.github.com>
2020-10-21 08:42:58 +0000

    Clean-up template test

--- tests/dcg/Generator/TemplateTest.php
+++ tests/dcg/Generator/TemplateTest.php
@@ -2,24 +2,55 @@
 
 namespace DrupalCodeGenerator\Tests\Generator;
 
+use DrupalCodeGenerator\Command\Template;
+use DrupalCodeGenerator\Test\GeneratorTest;
+
 /**
  * Test for template command.
  */
-final class TemplateTest extends BaseGeneratorTest {
+final class TemplateTest extends GeneratorTest {
 
-  protected $class = 'Template';
+  protected $fixtureDir = __DIR__;
 
-  protected $interaction = [
-    'Module name [%default_name%]:' => 'Example',
-    'Module machine name [example]:' => 'example',
-    'Template name [example]:' => 'foo',
-    'Create theme hook? [Yes]:' => 'Yes',
-    'Create preprocess hook? [Yes]:' => 'Yes',
-  ];
+  /**
+   * Test callback.
+   */
+  public function testGenerator(): void {
 
-  protected $fixtures = [
-    'example.module' => '/_template.module',
-    'templates/foo.html.twig' => '/_template.twig',
-  ];
+    $user_input = ['Example', 'example', 'foo', 'Yes', 'Yes'];
+    $this->execute(new Template(), $user_input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to template generator!
+    ––––––––––––––––––––––––––––––––
+
+     Module name [%default_name%]:
+     ➤ 
+
+     Module machine name [example]:
+     ➤ 
+
+     Template name [example]:
+     ➤ 
+
+     Create theme hook? [Yes]:
+     ➤ 
+
+     Create preprocess hook? [Yes]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.module
+     • templates/foo.html.twig
+
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.module', '_template.module');
+    $this->assertGeneratedFile('templates/foo.html.twig', '_template.twig');
+  }
 
 }
