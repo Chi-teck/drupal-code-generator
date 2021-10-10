d64558d49a4dcb812a2086143f337be6369b60eb
Chi-teck <Chi-teck@users.noreply.github.com>
2021-10-10 15:20:03 +0500

    Release 2.1.1

--- src/Application.php
+++ src/Application.php
@@ -29,7 +29,7 @@ class Application extends BaseApplication {
   /**
    * DCG version.
    */
-  public const VERSION = '2.1.0-dev';
+  public const VERSION = '2.1.1';
 
   /**
    * Path to templates directory.
