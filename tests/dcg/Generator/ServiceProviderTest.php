4972e56301963a97bf566202baf387eab555f1f9
Chi-teck <Chi-teck@users.noreply.github.com>
2021-10-14 14:36:17 +0500

    Update service provider test

--- tests/dcg/Generator/ServiceProviderTest.php
+++ tests/dcg/Generator/ServiceProviderTest.php
@@ -2,20 +2,44 @@
 
 namespace DrupalCodeGenerator\Tests\Generator;
 
+use DrupalCodeGenerator\Command\ServiceProvider;
+use DrupalCodeGenerator\Test\GeneratorTest;
+
 /**
- * Test for service:middleware command.
+ * Test for service-provider command.
  */
-final class ServiceProviderTest extends BaseGeneratorTest {
+final class ServiceProviderTest extends GeneratorTest {
 
-  protected string $class = 'ServiceProvider';
+  protected string $fixtureDir = __DIR__;
 
-  protected array $interaction = [
-    'Module name [%default_name%]:' => 'Example',
-    'Module machine name [example]:' => 'example',
-  ];
+  /**
+   * Test callback.
+   */
+  public function testGenerator(): void {
 
-  protected array $fixtures = [
-    'src/ExampleServiceProvider.php' => '/_service_provider.php',
-  ];
+    $user_input = ['Example', 'example', 'foo', 'Yes', 'Yes'];
+    $this->execute(new ServiceProvider(), $user_input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to service-provider generator!
+    ––––––––––––––––––––––––––––––––––––––––
+
+     Module name [%default_name%]:
+     ➤ 
+
+     Module machine name [example]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • src/ExampleServiceProvider.php
+
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('src/ExampleServiceProvider.php', '_service_provider.php');
+  }
 
 }
