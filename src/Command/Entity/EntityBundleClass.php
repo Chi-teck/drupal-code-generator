d169fca661bb4be434b862b8870f21dec7d07054
Moshe Weitzman <weitzman@tejasa.com>
2022-01-13 10:57:52 -0500

    Add 'All' choice for bundle selection in bundle class generator

--- src/Command/Entity/EntityBundleClass.php
+++ src/Command/Entity/EntityBundleClass.php
@@ -67,7 +67,17 @@ final class EntityBundleClass extends ModuleGenerator {
       static fn (array $bundle): string => (string) $bundle['label'],
       $bundle_info->getBundleInfo($vars['entity_type_id']),
     );
+    // Prepend an 'All' choice for user's convenience.
+    $bundle_choices_all = array_merge(['all' => 'All'], $bundle_choices);
     $vars['bundle_ids'] = $this->choice('Bundles, comma separated', $bundle_choices, NULL, TRUE);
+
+    if (in_array('all', $vars['bundle_ids']) && count($vars['bundle_ids']) >= 2) {
+        throw new \UnexpectedValueException("'All' may not be combined with other choices.");
+    }
+    if (in_array('all', $vars['bundle_ids'])) {
+        // Replace 'all' with all bundle IDs.
+        $vars['bundle_ids'] = array_keys($bundle_choices);
+    }
 
     $vars['classes'] = [];
     $vars['classes_fqn'] = [];
