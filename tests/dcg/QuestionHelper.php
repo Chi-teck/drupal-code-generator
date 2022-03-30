32c2b9a5fbcc0be83eba2b385bc4f56c8729d4a2
Alex Pott <alex.a.pott@googlemail.com>
2022-03-30 15:45:33 +0100

    Fix test code

--- tests/dcg/QuestionHelper.php
+++ tests/dcg/QuestionHelper.php
@@ -15,7 +15,7 @@ final class QuestionHelper extends BaseHelper {
   /**
    * {@inheritdoc}
    */
-  public function ask(InputInterface $input, OutputInterface $output, Question $question) {
+  public function ask(InputInterface $input, OutputInterface $output, Question $question): mixed {
     $question->setAutocompleterValues(NULL);
     $answer = parent::ask($input, $output, $question);
     // Write line after each input to make test output more readable.
