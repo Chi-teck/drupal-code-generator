ce01ca0f86771242de1287ade5285ce90f6e206a
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-02 14:06:41 +0500

    Create functional test for render-element generator

---
+++ tests/functional/Generator/RenderElementTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Generator;
+
+use DrupalCodeGenerator\Command\RenderElement;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Test for render-element command.
+ */
+final class RenderElementTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_render_element';
+
+  public function testGenerator(): void {
+
+    $this->execute(new RenderElement(), ['foo']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to render-element generator!
+    ––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • src/Element/Entity.php
+
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('src/Element/Entity.php');
+  }
+
+}
