ce0ffeb55d640e0f2c46bd9a1d2f1f6f45b3db6f
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-02 18:38:06 +0500

    Add functional test for template generator

---
+++ tests/functional/Generator/TemplateTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Generator;
+
+use DrupalCodeGenerator\Command\Template;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * A test for template command.
+ */
+final class TemplateTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_template';
+
+  public function testGenerator(): void {
+
+    $user_input = ['example', 'foo', 'Yes', 'Yes'];
+    $this->execute(new Template(), $user_input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to template generator!
+    ––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Template name [example]:
+     ➤ 
+
+     Create theme hook? [Yes]:
+     ➤ 
+
+     Create preprocess hook? [Yes]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.module
+     • templates/foo.html.twig
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.module');
+    $this->assertGeneratedFile('templates/foo.html.twig');
+  }
+
+}
