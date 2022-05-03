3dc4a56794f707e2d2636df5e44734bd4e798123
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-03 14:52:47 +0500

    Add functional test for form:config generator

---
+++ tests/functional/Generator/Form/ConfigTest.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Form;
+
+use DrupalCodeGenerator\Command\Form\Config;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests form:config generator.
+ */
+final class ConfigTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_config';
+
+  public function testGenerator(): void {
+    $input = [
+      'foo',
+      'SettingsForm',
+      'Yes',
+      'foo.example',
+      '/admin/config/content/example',
+      'Yo',
+      'administer site configuration',
+      'Yes',
+      'Foo settings',
+      'Configure foo.',
+      'system.admin_config_content',
+    ];
+    $this->execute(new Config(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to config-form generator!
+    –––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Class [SettingsForm]:
+     ➤ 
+
+     Would you like to create a route for this form? [Yes]:
+     ➤ 
+
+     Route name [foo.settings]:
+     ➤ 
+
+     Route path [/admin/config/system/settings]:
+     ➤ 
+
+     Route title [Settings]:
+     ➤ 
+
+     Route permission [administer site configuration]:
+     ➤ 
+
+     Would you like to create a menu link for this route? [Yes]:
+     ➤ 
+
+     Link title [Yo]:
+     ➤ 
+
+     Link description:
+     ➤ 
+
+     Parent menu item [system.admin_config_content]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • foo.links.menu.yml
+     • foo.routing.yml
+     • config/schema/foo.schema.yml
+     • src/Form/SettingsForm.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('foo.links.menu.yml');
+    $this->assertGeneratedFile('foo.routing.yml');
+    $this->assertGeneratedFile('config/schema/foo.schema.yml');
+    $this->assertGeneratedFile('src/Form/SettingsForm.php');
+  }
+
+}
---
+++ tests/functional/Generator/Form/ConfirmTest.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Form;
+
+use DrupalCodeGenerator\Command\Form\Confirm;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests form:confirm generator.
+ */
+final class ConfirmTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_confirm';
+
+  public function testGenerator(): void {
+    $input = [
+      'foo',
+      'ExampleConfirmForm',
+      'Yes',
+      'foo.example',
+      '/foo/example',
+      'Wow',
+      'administer site configuration',
+    ];
+    $this->execute(new Confirm(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to confirm-form generator!
+    ––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Class [ExampleConfirmForm]:
+     ➤ 
+
+     Would you like to create a route for this form? [Yes]:
+     ➤ 
+
+     Route name [foo.example_confirm]:
+     ➤ 
+
+     Route path [/foo/example-confirm]:
+     ➤ 
+
+     Route title [Example confirm]:
+     ➤ 
+
+     Route permission [administer site configuration]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • foo.routing.yml
+     • src/Form/ExampleConfirmForm.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('foo.routing.yml');
+    $this->assertGeneratedFile('src/Form/ExampleConfirmForm.php');
+  }
+
+}
