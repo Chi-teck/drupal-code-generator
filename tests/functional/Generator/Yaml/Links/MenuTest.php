2689af6e51e3b1b4c707ddb845241b2cddc62074
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-04 17:16:42 +0500

    Add functional test for yml:links:task generator

---
+++ tests/functional/Generator/Yaml/Links/MenuTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Yaml\Links;
+
+use DrupalCodeGenerator\Command\Yml\Links\Menu;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests yml:menu:links generator.
+ */
+class MenuTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_menu';
+
+  public function testGenerator(): void {
+
+    $this->execute(new Menu(), ['example']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to menu-links generator!
+    ––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.links.menu.yml
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.links.menu.yml');
+  }
+
+}
---
+++ tests/functional/Generator/Yaml/Links/TaskTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Yaml\Links;
+
+use DrupalCodeGenerator\Command\Yml\Links\Task;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests yml:links:task generator.
+ */
+class TaskTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_task';
+
+  public function testGenerator(): void {
+
+    $this->execute(new Task(), ['example']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to task-links generator!
+    ––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.links.task.yml
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.links.task.yml');
+  }
+
+}
