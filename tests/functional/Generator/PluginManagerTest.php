657d71b0ffc8f2209000e1aecb6a24ab4568a2ba
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-04 07:38:35 +0500

    Add functional test for plugin manager generator

---
+++ tests/functional/Generator/PluginManagerTest.php
@@ -0,0 +1,149 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator;
+
+use DrupalCodeGenerator\Command\PluginManager;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests plugin-manager generator.
+ */
+final class PluginManagerTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_plugin_manager';
+
+  public function testAnnotationDiscovery(): void {
+    $input = [
+      'foo',
+      'bar',
+      'Annotation',
+    ];
+    $this->execute(new PluginManager(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to plugin-manager generator!
+    ––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Plugin type [foo]:
+     ➤ 
+
+     Discovery type [Annotation]:
+      [1] Annotation
+      [2] YAML
+      [3] Hook
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • foo.services.yml
+     • src/BarInterface.php
+     • src/BarPluginBase.php
+     • src/BarPluginManager.php
+     • src/Annotation/Bar.php
+     • src/Plugin/Bar/Foo.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->fixtureDir .= '/_annotation';
+    $this->assertGeneratedFile('foo.services.yml');
+    $this->assertGeneratedFile('src/BarInterface.php');
+    $this->assertGeneratedFile('src/BarPluginBase.php');
+    $this->assertGeneratedFile('src/BarPluginManager.php');
+    $this->assertGeneratedFile('src/Annotation/Bar.php');
+    $this->assertGeneratedFile('src/Plugin/Bar/Foo.php');
+  }
+
+  public function testYamlDiscovery(): void {
+    $input = [
+      'foo',
+      'bar',
+      'YAML',
+    ];
+    $this->execute(new PluginManager(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to plugin-manager generator!
+    ––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Plugin type [foo]:
+     ➤ 
+
+     Discovery type [Annotation]:
+      [1] Annotation
+      [2] YAML
+      [3] Hook
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • foo.bars.yml
+     • foo.services.yml
+     • src/BarDefault.php
+     • src/BarInterface.php
+     • src/BarPluginManager.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->fixtureDir .= '/_yaml';
+    $this->assertGeneratedFile('foo.bars.yml');
+    $this->assertGeneratedFile('foo.services.yml');
+    $this->assertGeneratedFile('src/BarDefault.php');
+    $this->assertGeneratedFile('src/BarInterface.php');
+    $this->assertGeneratedFile('src/BarPluginManager.php');
+  }
+
+  public function testHookDiscovery(): void {
+    $input = [
+      'foo',
+      'bar',
+      'Hook',
+    ];
+    $this->execute(new PluginManager(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to plugin-manager generator!
+    ––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Plugin type [foo]:
+     ➤ 
+
+     Discovery type [Annotation]:
+      [1] Annotation
+      [2] YAML
+      [3] Hook
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • foo.module
+     • foo.services.yml
+     • src/BarDefault.php
+     • src/BarInterface.php
+     • src/BarPluginManager.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->fixtureDir .= '/_hook';
+    $this->assertGeneratedFile('foo.module');
+    $this->assertGeneratedFile('foo.services.yml');
+    $this->assertGeneratedFile('src/BarDefault.php');
+    $this->assertGeneratedFile('src/BarInterface.php');
+    $this->assertGeneratedFile('src/BarPluginManager.php');
+  }
+
+}
