63b7899ad4d1a15dc8175489e63e573770a442ac
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-04 17:10:40 +0500

    Add functional test for yml:links:contextual generator

---
+++ tests/functional/Generator/Yaml/Links/ContextualTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Yaml\Links;
+
+use DrupalCodeGenerator\Command\Yml\Links\Contextual;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests yml:links:contextual generator.
+ */
+class ContextualTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_contextual';
+
+  public function testGenerator(): void {
+
+    $this->execute(new Contextual(), ['example']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to contextual-links generator!
+    ––––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.links.contextual.yml
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.links.contextual.yml');
+  }
+
+}
