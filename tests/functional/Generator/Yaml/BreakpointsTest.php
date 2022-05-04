a784ce18cb0df08c66e960ca181c3e8a076b91b2
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-04 11:13:39 +0500

    Add functional test for yml:breakpoints generator

---
+++ tests/functional/Generator/Yaml/BreakpointsTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Yaml;
+
+use DrupalCodeGenerator\Command\Yml\Breakpoints;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests yml:breakpoints generator.
+ */
+final class BreakpointsTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_breakpoints';
+
+  public function testGenerator(): void {
+    $input = [
+      'example',
+      'garland',
+      'Example description.',
+      'Custom',
+    ];
+    $this->execute(new Breakpoints(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to breakpoints generator!
+    –––––––––––––––––––––––––––––––––––
+
+     Theme machine name [%default_name%]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.breakpoints.yml
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.breakpoints.yml');
+  }
+
+}
