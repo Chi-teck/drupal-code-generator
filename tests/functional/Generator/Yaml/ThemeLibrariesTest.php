dcd65534c8d826f36c8be6cc56c56a05e3b1c208
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-04 07:45:20 +0500

    Add functional test for yml:theme-libraries generator

---
+++ tests/functional/Generator/Yaml/ThemeLibrariesTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Yaml;
+
+use DrupalCodeGenerator\Command\Yml\ThemeLibraries;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests yml:theme-libraries generator.
+ */
+final class ThemeLibrariesTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_theme_libraries';
+
+  public function testGenerator(): void {
+    $this->execute(new ThemeLibraries(), ['example']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to theme-libraries generator!
+    –––––––––––––––––––––––––––––––––––––––
+
+     Theme machine name [%default_name%]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.libraries.yml
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.libraries.yml');
+  }
+
+}
