2e5b7f3611cb667c83f1200d64feef3e19c36f0a
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-06 08:22:24 +0500

    Add functional test for twig extension generator

---
+++ tests/functional/Generator/Service/TwigExtensionTest.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator;
+
+use DrupalCodeGenerator\Command\Service\TwigExtension;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests service:twig-extension generator.
+ */
+final class TwigExtensionTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_twig_extension';
+
+  public function testGenerator(): void {
+    $input = [
+      'example',
+      'ExampleTwigExtension',
+      'Yes',
+      'entity_type.manager',
+      '',
+    ];
+    $this->execute(new TwigExtension(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to twig-extension generator!
+    ––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Class [ExampleTwigExtension]:
+     ➤ 
+
+     Would you like to inject dependencies? [Yes]:
+     ➤ 
+
+     Type the service name or use arrows up/down. Press enter to continue:
+     ➤ 
+
+     Type the service name or use arrows up/down. Press enter to continue:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.services.yml
+     • src/ExampleTwigExtension.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.services.yml');
+    $this->assertGeneratedFile('src/ExampleTwigExtension.php');
+  }
+
+}
