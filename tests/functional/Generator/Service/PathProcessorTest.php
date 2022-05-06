69e926d3156178ff11270f509e39a1c3ceb88944
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-06 08:48:02 +0500

    Add functional test for path processor generator

---
+++ tests/functional/Generator/Service/PathProcessorTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Service;
+
+use DrupalCodeGenerator\Command\Service\PathProcessor;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests service:path-processor generator.
+ */
+final class PathProcessorTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_path_processor';
+
+  public function testGenerator(): void {
+
+    $this->execute(new PathProcessor(), ['example', 'PathProcessorExample']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to path-processor generator!
+    ––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Class [PathProcessorExample]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.services.yml
+     • src/PathProcessor/PathProcessorExample.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.services.yml');
+    $this->assertGeneratedFile('src/PathProcessor/PathProcessorExample.php');
+  }
+
+}
