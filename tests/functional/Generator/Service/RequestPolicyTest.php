5d6553332f7a96fd82933a15e7fc10a75a947a8c
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-06 08:39:18 +0500

    Add functional test for cache policy generators

---
+++ tests/functional/Generator/Service/RequestPolicyTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Service;
+
+use DrupalCodeGenerator\Command\Service\RequestPolicy;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests service:request-policy generator.
+ */
+final class RequestPolicyTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_request_policy';
+
+  public function testGenerator(): void {
+
+    $this->execute(new RequestPolicy(), ['foo', 'Example']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to request-policy generator!
+    ––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Class [Example]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • foo.services.yml
+     • src/PageCache/Example.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('foo.services.yml');
+    $this->assertGeneratedFile('src/PageCache/Example.php');
+  }
+
+}
---
+++ tests/functional/Generator/Service/ResponsePolicyTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Service;
+
+use DrupalCodeGenerator\Command\Service\ResponsePolicy;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests service:response-policy generator.
+ */
+final class ResponsePolicyTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_response_policy';
+
+  public function testGenerator(): void {
+
+    $this->execute(new ResponsePolicy(), ['foo', 'Example']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to response-policy generator!
+    –––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Class [Example]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • foo.services.yml
+     • src/PageCache/Example.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('foo.services.yml');
+    $this->assertGeneratedFile('src/PageCache/Example.php');
+  }
+
+}
