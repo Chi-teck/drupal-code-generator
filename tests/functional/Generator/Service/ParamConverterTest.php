b0c6b06b6de0b6fc5f9711efd7c4b5dfa975385f
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-07 10:25:44 +0500

    Add functional test for param converter generator

---
+++ tests/functional/Generator/Service/ParamConverterTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Service;
+
+use DrupalCodeGenerator\Command\Service\ParamConverter;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests service:param-converter generator.
+ */
+final class ParamConverterTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_param_converter';
+
+  public function testGenerator(): void {
+    $input = [
+      'example',
+      'foo',
+      'FooParamConverter',
+    ];
+    $this->execute(new ParamConverter(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to param-converter generator!
+    –––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Parameter type [example]:
+     ➤ 
+
+     Class [FooParamConverter]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.services.yml
+     • src/FooParamConverter.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.services.yml');
+    $this->assertGeneratedFile('src/FooParamConverter.php');
+  }
+
+}
