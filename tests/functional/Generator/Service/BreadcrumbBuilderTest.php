e751d8f18bff767f5e0c087ee8df1b8b71c0f323
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-08 10:07:42 +0500

    Add functional test for breadcrumb builder generator

---
+++ tests/functional/Generator/Service/BreadcrumbBuilderTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Generator\Service;
+
+use DrupalCodeGenerator\Command\Service\BreadcrumbBuilder;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests breadcrumb-builder generator.
+ */
+final class BreadcrumbBuilderTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_breadcrumb_builder';
+
+  public function testGenerator(): void {
+
+    $this->execute(new BreadcrumbBuilder(), ['example', 'ExampleBreadcrumbBuilder']);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to breadcrumb-builder generator!
+    ––––––––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Class [ExampleBreadcrumbBuilder]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • example.services.yml
+     • src/ExampleBreadcrumbBuilder.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('example.services.yml');
+    $this->assertGeneratedFile('src/ExampleBreadcrumbBuilder.php');
+  }
+
+}
