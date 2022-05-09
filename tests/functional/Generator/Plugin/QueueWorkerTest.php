bd95026ecefe43645e94548392d5c1edee689ae3
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-09 09:17:51 +0500

    Add functional test for queue worker generator

---
+++ tests/functional/Generator/Plugin/QueueWorkerTest.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Plugin;
+
+use DrupalCodeGenerator\Command\Plugin\QueueWorker;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests plugin:queue-worker generator.
+ */
+final class QueueWorkerTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_queue_worker';
+
+  public function testGenerator(): void {
+    $input = [
+      'example',
+      'Test',
+      'example_foo_bar',
+      'FooBar',
+    ];
+    $this->execute(new QueueWorker(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to queue-worker generator!
+    ––––––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Plugin label [Example]:
+     ➤ 
+
+     Plugin ID [example_test]:
+     ➤ 
+
+     Plugin class [FooBar]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • src/Plugin/QueueWorker/FooBar.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('src/Plugin/QueueWorker/FooBar.php');
+  }
+
+}
