8cd6cd917b36a0829b84fb6018f1641010b9f786
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-09 09:23:10 +0500

    Add functional test for menu-link generator

---
+++ tests/functional/Generator/Plugin/MenuLinkTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Plugin;
+
+use DrupalCodeGenerator\Command\Plugin\MenuLink;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests plugin:menu-link generator.
+ */
+final class MenuLinkTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_menu_link';
+
+  public function testGenerator(): void {
+    $input = [
+      'example',
+      'FooMenuLink',
+    ];
+    $this->execute(new MenuLink(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to menu-link generator!
+    –––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Class [ExampleMenuLink]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • src/Plugin/Menu/FooMenuLink.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('src/Plugin/Menu/FooMenuLink.php');
+  }
+
+}
