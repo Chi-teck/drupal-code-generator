2fde7afb7bb3784940554260152dc475e3b647ab
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-11 08:41:22 +0500

    Add functional test for validation condition generator

---
+++ tests/functional/Generator/Plugin/ConditionTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Plugin;
+
+use DrupalCodeGenerator\Command\Plugin\Condition;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests plugin:condition generator.
+ */
+final class ConditionTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_condition';
+
+  public function testGenerator(): void {
+    $input = [
+      'foo',
+      'Example',
+      'foo_example',
+      'Example',
+    ];
+    $this->execute(new Condition(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to condition generator!
+    –––––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Plugin label [Example]:
+     ➤ 
+
+     Plugin ID [foo_example]:
+     ➤ 
+
+     Plugin class [Example]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • config/schema/foo.schema.yml
+     • src/Plugin/Condition/Example.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('config/schema/foo.schema.yml');
+    $this->assertGeneratedFile('src/Plugin/Condition/Example.php');
+  }
+
+}
