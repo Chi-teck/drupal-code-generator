97da1b613cf96aa47dad4c4df6f5f0965310b5e6
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-16 10:00:27 +0500

    Add functional test for action plugin generator

---
+++ tests/functional/Generator/Plugin/ActionTest.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Plugin;
+
+use DrupalCodeGenerator\Command\Plugin\Action;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests plugin:action generator.
+ */
+final class ActionTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_action';
+
+  public function testGenerator(): void {
+    $input = [
+      'example',
+      'Foo',
+      'example_foo',
+      'Foo',
+      'Custom',
+      'yes',
+    ];
+    $this->execute(new Action(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to action generator!
+    ––––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Action label [Update node title]:
+     ➤ 
+
+     Plugin ID [example_foo]:
+     ➤ 
+
+     Plugin class [Foo]:
+     ➤ 
+
+     Action category [Custom]:
+     ➤ 
+
+     Make the action configurable? [No]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • config/schema/example.schema.yml
+     • src/Plugin/Action/Foo.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('config/schema/example.schema.yml');
+    $this->assertGeneratedFile('src/Plugin/Action/Foo.php');
+  }
+
+}
