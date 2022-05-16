fd4336f7b93e489d2d097be94d00b5620988c5a5
Chi-teck <Chi-teck@users.noreply.github.com>
2022-05-16 10:16:44 +0500

    Add functional test for block plugin generator

---
+++ tests/functional/Generator/Plugin/BlockTest.php
@@ -0,0 +1,77 @@
+<?php declare(strict_types=1);
+
+namespace DrupalCodeGenerator\Tests\Functional\Generator\Plugin;
+
+use DrupalCodeGenerator\Command\Plugin\Block;
+use DrupalCodeGenerator\Test\Functional\GeneratorTestBase;
+
+/**
+ * Tests plugin:block generator.
+ */
+final class BlockTest extends GeneratorTestBase {
+
+  protected string $fixtureDir = __DIR__ . '/_block';
+
+  public function testGenerator(): void {
+    $input = [
+      'foo',
+      'Example',
+      'foo_example',
+      'ExampleBlock',
+      'Custom',
+      'Yes',
+      'Yes',
+      'cron',
+      '',
+      'Yes',
+    ];
+    $this->execute(new Block(), $input);
+
+    $expected_display = <<< 'TXT'
+
+     Welcome to block generator!
+    –––––––––––––––––––––––––––––
+
+     Module machine name [%default_name%]:
+     ➤ 
+
+     Block admin label [Example]:
+     ➤ 
+
+     Plugin ID [foo_example]:
+     ➤ 
+
+     Plugin class [ExampleBlock]:
+     ➤ 
+
+     Block category [Custom]:
+     ➤ 
+
+     Make the block configurable? [No]:
+     ➤ 
+
+     Would you like to inject dependencies? [No]:
+     ➤ 
+
+     Type the service name or use arrows up/down. Press enter to continue:
+     ➤ 
+
+     Type the service name or use arrows up/down. Press enter to continue:
+     ➤ 
+
+     Create access callback? [No]:
+     ➤ 
+
+     The following directories and files have been created or updated:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+     • config/schema/foo.schema.yml
+     • src/Plugin/Block/ExampleBlock.php
+
+    TXT;
+    $this->assertDisplay($expected_display);
+
+    $this->assertGeneratedFile('config/schema/foo.schema.yml');
+    $this->assertGeneratedFile('src/Plugin/Block/ExampleBlock.php');
+  }
+
+}
