4f210137140d32992e6fbb8f28e2167380e3c009
Chi-teck <Chi-teck@users.noreply.github.com>
2022-06-05 17:20:47 +0500

    Clean-up functional-tests.sh

--- scripts/functional-tests.sh
+++ scripts/functional-tests.sh
@@ -2,8 +2,7 @@
 # shellcheck disable=SC2086
 
 # === Configuration. === #
-set -o errexit
-set -o nounset
+set -Eeuo pipefail
 
 ROOT_DIR=$(realpath "$(dirname $0)")/..
 
@@ -11,11 +10,10 @@ DRUPAL_VERSION=${DRUPAL_VERSION:-}
 if [[ -z $DRUPAL_VERSION ]]; then
   DRUPAL_VERSION=$(git ls-remote -h https://git.drupalcode.org/project/drupal.git | grep -o '10\..\.x' | tail -n1)
 fi
-DRUPAL_DIR=${DRUPAL_DIR:-/tmp/dcg_functional/build}
-DRUPAL_CACHE_DIR=${DRUPAL_CACHE_DIR:-/tmp/dcg_functional/cache/$DRUPAL_VERSION}
+WORKSPACE_DIR=${WORKSPACE_DIR:-/tmp/dcg_functional}
+DRUPAL_DIR=$WORKSPACE_DIR/drupal
+CACHE_DIR=$WORKSPACE_DIR/cache
 DCG_DIR=$DRUPAL_DIR/vendor/chi-teck/drupal-code-generator
-
-TARGET_TEST=${1:-all}
 
 echo -----------------------------------------------
 echo ' DRUPAL PATH:   ' $DRUPAL_DIR
@@ -24,37 +22,31 @@ echo ' DCG DIR:       ' $DCG_DIR
 echo ' ROOT DIR:      ' $ROOT_DIR
 echo -----------------------------------------------
 
-# === Helper functions. === #
-
-function dcg_label {
-  echo -e "\n\e[30;43m -= $* =- \e[0m\n"
-}
-
-function dcg_phpunit {
-  $DRUPAL_DIR/vendor/bin/phpunit -c $DCG_DIR --testsuite=functional
-}
-
-# === Create a site under testing. === #
 if [[ -d $DRUPAL_DIR ]]; then
   chmod -R 777 $DRUPAL_DIR
   rm -rf $DRUPAL_DIR
 fi
 
-if [[ -d $DRUPAL_CACHE_DIR ]]; then
-  cp -r $DRUPAL_CACHE_DIR $DRUPAL_DIR
+if [[ -d $CACHE_DIR/$DRUPAL_VERSION ]]; then
+  echo '🚩 Install Drupal from cache'
+  cp -r $CACHE_DIR/$DRUPAL_VERSION $DRUPAL_DIR
 else
-  export COMPOSER_PROCESS_TIMEOUT=1900
+  echo '🚩 Clone Drupal core'
   git clone --depth 1 --branch $DRUPAL_VERSION  https://git.drupalcode.org/project/drupal.git $DRUPAL_DIR
+  echo '🚩 Install Composer dependencies'
   composer -d$DRUPAL_DIR install
-  mkdir -m 777 $DRUPAL_DIR/sites/default/files
+  echo '🚩 Install local DCG'
   composer -d$DRUPAL_DIR config repositories.dcg path "$ROOT_DIR"
   composer -d$DRUPAL_DIR require chi-teck/drupal-code-generator --ignore-platform-req=php
+  echo '🚩 Install Drupal'
+  mkdir -m 777 $DRUPAL_DIR/sites/default/files
   php $DRUPAL_DIR/core/scripts/drupal install standard
-  mkdir -p $DRUPAL_CACHE_DIR
-  cp -r $DRUPAL_DIR/. $DRUPAL_CACHE_DIR
+  echo '🚩 Update cache'
+  if [[ ! -d $CACHE_DIR ]]; then
+    mkdir -p $CACHE_DIR
+  fi
+  cp -r $DRUPAL_DIR $CACHE_DIR/$DRUPAL_VERSION
 fi
 
-# === Tests === #
-dcg_label 'TESTS'
-dcg_phpunit
-
+echo '🚩 Run tests'
+$DRUPAL_DIR/vendor/bin/phpunit -c $DCG_DIR --testsuite=functional
