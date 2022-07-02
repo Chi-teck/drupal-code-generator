83a222d017635f6dee0e91be495a4cc4489d84a5
Chi-teck <Chi-teck@users.noreply.github.com>
2022-07-02 15:55:41 +0500

    Rename getHooks() method

--- src/Helper/Drupal/HookInfo.php
+++ src/Helper/Drupal/HookInfo.php
@@ -36,22 +36,26 @@ final class HookInfo extends Helper {
     }
 
     $hooks = self::parseHooks(\DRUPAL_ROOT . '/core/core.api.php');
+    dump($hooks);
 
-    $api_files = \glob(\DRUPAL_ROOT . '/core/lib/Drupal/Core/*/*.api.php');
-    foreach ($api_files as $api_file) {
-      if (\file_exists($api_file)) {
-        $hooks = \array_merge($hooks, self::parseHooks($api_file));
-      }
-    }
+    $core_api_files = \glob(\DRUPAL_ROOT . '/core/lib/Drupal/Core/*/*.api.php');
+    $core_api_files[] = \DRUPAL_ROOT . '/core/core.api.php';
 
+    $module_api_files = [];
     foreach ($this->moduleHandler->getModuleList() as $machine_name => $module) {
       $api_file = \DRUPAL_ROOT . '/' . $module->getPath() . '/' . $machine_name . '.api.php';
       if (\file_exists($api_file)) {
-        $hooks = \array_merge($hooks, self::parseHooks($api_file));
+        $module_api_files[] = $api_file;
       }
     }
 
-    return $hooks;
+    $templates = [];
+    $api_files = \array_merge($core_api_files, $module_api_files);
+    foreach ($api_files as $api_file) {
+      $templates = \array_merge($hooks, self::parseHooks($api_file));
+    }
+
+    return $templates;
   }
 
   /**
@@ -98,80 +102,61 @@ final class HookInfo extends Helper {
   }
 
   /**
-   * Returns file type of the hook.
+   * Returns filetype of a hook.
    */
   public static function getFileType(string $hook_name): string {
-
-    // Drupal hooks that are not situated in MODULE_NAME.module file.
-    $special_hooks = [
-      'install' => [
-        'install',
-        'uninstall',
-        'schema',
-        'requirements',
-        'update_N',
-        'update_last_removed',
-      ],
+    // Some Drupal hooks are not defined in MODULE_NAME.module file.
+    return match ($hook_name) {
+      'install',
+      'uninstall',
+      'schema',
+      'requirements',
+      'update_N',
+      'update_last_removed' => 'install',
       // See views_hook_info().
-      'views.inc' => [
-        'views_data',
-        'views_data_alter',
-        'views_analyze',
-        'views_invalidate_cache',
-        'field_views_data',
-        'field_views_data_alter',
+      'views_data',
+      'views_data_alter',
+      'views_analyze',
+      'views_invalidate_cache',
+      'field_views_data',
+      'field_views_data_alter',
         // See \Drupal\views\views::$plugins.
-        'views_plugins_access_alter',
-        'views_plugins_area_alter',
-        'views_plugins_argument_alter',
-        'views_plugins_argument_default_alter',
-        'views_plugins_argument_validator_alter',
-        'views_plugins_cache_alter',
-        'views_plugins_display_extender_alter',
-        'views_plugins_display_alter',
-        'views_plugins_exposed_form_alter',
-        'views_plugins_field_alter',
-        'views_plugins_filter_alter',
-        'views_plugins_join_alter',
-        'views_plugins_pager_alter',
-        'views_plugins_query_alter',
-        'views_plugins_relationship_alter',
-        'views_plugins_row_alter',
-        'views_plugins_sort_alter',
-        'views_plugins_style_alter',
-        'views_plugins_wizard_alter',
-      ],
-      'views_execution.inc' => [
-        'views_query_substitutions',
-        'views_form_substitutions',
-        'views_pre_view',
-        'views_pre_build',
-        'views_post_build',
-        'views_pre_execute',
-        'views_post_execute',
-        'views_pre_render',
-        'views_post_render',
-        'views_query_alter',
-      ],
-      // See system_hook_info().
-      'tokens.inc' => [
-        'token_info',
-        'token_info_alter',
-        'tokens',
-        'tokens_alter',
-      ],
-      'post_update.php' => [
-        'post_update_N',
-      ],
-    ];
-
-    foreach ($special_hooks as $group => $hooks) {
-      if (\in_array($hook_name, $hooks)) {
-        return $group;
-      }
-    }
-
-    return 'module';
+      'views_plugins_access_alter',
+      'views_plugins_area_alter',
+      'views_plugins_argument_alter',
+      'views_plugins_argument_default_alter',
+      'views_plugins_argument_validator_alter',
+      'views_plugins_cache_alter',
+      'views_plugins_display_extender_alter',
+      'views_plugins_display_alter',
+      'views_plugins_exposed_form_alter',
+      'views_plugins_field_alter',
+      'views_plugins_filter_alter',
+      'views_plugins_join_alter',
+      'views_plugins_pager_alter',
+      'views_plugins_query_alter',
+      'views_plugins_relationship_alter',
+      'views_plugins_row_alter',
+      'views_plugins_sort_alter',
+      'views_plugins_style_alter',
+      'views_plugins_wizard_alter' => 'views.inc',
+      'views_query_substitutions',
+      'views_form_substitutions',
+      'views_pre_view',
+      'views_pre_build',
+      'views_post_build',
+      'views_pre_execute',
+      'views_post_execute',
+      'views_pre_render',
+      'views_post_render',
+      'views_query_alter' => 'views_execution.inc',
+      'token_info',
+      'token_info_alter',
+      'tokens',
+      'tokens_alter' => 'tokens.inc',
+      'post_update_N' => 'post_update.php',
+      default => 'module',
+    };
   }
 
 }
