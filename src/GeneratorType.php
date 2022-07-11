ded3505fd53d8134a8e710dd1bcf80ce5089ebb0
Chi-teck <Chi-teck@users.noreply.github.com>
2022-07-11 15:53:29 +0500

    Update GeneratorType test

--- src/GeneratorType.php
+++ src/GeneratorType.php
@@ -11,16 +11,23 @@ namespace DrupalCodeGenerator;
  */
 enum GeneratorType {
 
+  // @todo Support installation profiles.
   case MODULE;
   case MODULE_COMPONENT;
   case THEME;
   case THEME_COMPONENT;
   case OTHER;
 
+  /**
+   * Determines if the generator produces a new Drupal extension.
+   */
   public function isNewExtension(): bool {
     return $this === self::MODULE || $this === self::THEME;
   }
 
+  /**
+   * Defines a label for extension human name.
+   */
   public function getNameLabel(): string {
     return match ($this) {
       self::MODULE, self::MODULE_COMPONENT => 'Module name',
@@ -29,6 +36,9 @@ enum GeneratorType {
     };
   }
 
+  /**
+   * Defines a label for extension machine name.
+   */
   public function getMachineNameLabel(): string {
     return match ($this) {
       self::MODULE, self::MODULE_COMPONENT => 'Module machine name',
--- tests/unit/GeneratorTypeTest.php
+++ tests/unit/GeneratorTypeTest.php
@@ -10,12 +10,37 @@ use PHPUnit\Framework\TestCase;
  */
 final class GeneratorTypeTest extends TestCase {
 
-  public function testGeneratorType(): void {
+  /**
+   * Test callback.
+   */
+  public function testIsNewExtension(): void {
     self::assertTrue(GeneratorType::MODULE->isNewExtension());
     self::assertFalse(GeneratorType::MODULE_COMPONENT->isNewExtension());
     self::assertTrue(GeneratorType::THEME->isNewExtension());
     self::assertFalse(GeneratorType::THEME_COMPONENT->isNewExtension());
     self::assertFalse(GeneratorType::OTHER->isNewExtension());
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testGetNameLabel(): void {
+    self::assertSame('Module name', GeneratorType::MODULE->getNameLabel());
+    self::assertSame('Module name', GeneratorType::MODULE_COMPONENT->getNameLabel());
+    self::assertSame('Theme name', GeneratorType::THEME->getNameLabel());
+    self::assertSame('Theme name', GeneratorType::THEME_COMPONENT->getNameLabel());
+    self::assertSame('Name', GeneratorType::OTHER->getNameLabel());
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testGetMachineNameLabel(): void {
+    self::assertSame('Module machine name', GeneratorType::MODULE->getMachineNameLabel());
+    self::assertSame('Module machine name', GeneratorType::MODULE_COMPONENT->getMachineNameLabel());
+    self::assertSame('Theme machine name', GeneratorType::THEME->getMachineNameLabel());
+    self::assertSame('Theme machine name', GeneratorType::THEME_COMPONENT->getMachineNameLabel());
+    self::assertSame('Machine name', GeneratorType::OTHER->getMachineNameLabel());
   }
 
 }
