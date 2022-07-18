cef708a985927e7d2ea0e6f512d6539a7815a4d0
Chi-teck <Chi-teck@users.noreply.github.com>
2022-07-18 14:16:26 +0500

    Clean-up template generator

--- src/Command/Template.php
+++ src/Command/Template.php
@@ -21,17 +21,37 @@ final class Template extends BaseGenerator {
     $vars['machine_name'] = $ir->askMachineName();
     $vars['name'] = $ir->askName();
 
-    $vars['template_name'] = $ir->ask('Template name', 'example');
+    $vars['template_name'] = $ir->ask('Template name', validator: [self::class, 'validateTemplateName']);
+    if (!\str_ends_with($vars['template_name'], '.twig')) {
+      $vars['template_name'] .= '.html.twig';
+    }
+
+    $vars['theme_key'] = \preg_replace(
+      ['#(\.html)?\.twig$#', '#[^a-z\d]#'],
+      ['', '_'],
+      $vars['template_name'],
+    );
+
     $vars['create_theme'] = $ir->confirm('Create theme hook?');
     $vars['create_preprocess'] = $ir->confirm('Create preprocess hook?');
 
-    $assets->addFile('templates/{template_name}.html.twig', 'template');
+    $assets->addFile('templates/{template_name}', 'template');
 
     if ($vars['create_theme'] || $vars['create_preprocess']) {
       $assets->addFile('{machine_name}.module')
         ->template('module')
         ->appendIfExists(7);
     }
+  }
+
+  /**
+   * Validates template name.
+   */
+  public static function validateTemplateName(mixed $value): string {
+    if (!\is_string($value) || !\preg_match('/^[a-z\d][a-z\d\.\-]*[a-z\d]$/', $value)) {
+      throw new \UnexpectedValueException('The value is not correct template name.');
+    }
+    return $value;
   }
 
 }
--- tests/functional/Generator/TemplateTest.php
+++ tests/functional/Generator/TemplateTest.php
@@ -12,9 +12,12 @@ final class TemplateTest extends GeneratorTestBase {
 
   protected string $fixtureDir = __DIR__ . '/_template';
 
+  /**
+   * Test callback.
+   */
   public function testGenerator(): void {
 
-    $user_input = ['example', 'Example', 'foo', 'Yes', 'Yes'];
+    $user_input = ['example', 'Example', 'section', 'Yes', 'Yes'];
     $this->execute(Template::class, $user_input);
 
     $expected_display = <<< 'TXT'
@@ -28,7 +31,7 @@ final class TemplateTest extends GeneratorTestBase {
      Module name [Example]:
      ➤ 
 
-     Template name [example]:
+     Template name:
      ➤ 
 
      Create theme hook? [Yes]:
@@ -40,13 +43,47 @@ final class TemplateTest extends GeneratorTestBase {
      The following directories and files have been created or updated:
     –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
      • example.module
-     • templates/foo.html.twig
+     • templates/section.html.twig
 
     TXT;
     $this->assertDisplay($expected_display);
 
     $this->assertGeneratedFile('example.module');
-    $this->assertGeneratedFile('templates/foo.html.twig');
+    $this->assertGeneratedFile('templates/section.html.twig');
+  }
+
+  /**
+   * Test callback.
+   *
+   * @dataProvider templateNameValidatorProvider()
+   */
+  public function testTemplateNameValidator(mixed $value, bool $exception): void {
+    if ($exception) {
+      self::expectExceptionObject(new \UnexpectedValueException('The value is not correct template name'));
+    }
+    $result = Template::validateTemplateName($value);
+    self::assertSame($value, $result);
+
+  }
+
+  /**
+   * Data provider callback for testTemplateNameValidator().
+   */
+  public function templateNameValidatorProvider(): array {
+    return [
+      ['aaa', FALSE],
+      ['aaa.bbb', FALSE],
+      ['aaa.bbb.html', FALSE],
+      ['aaa.bbb.html.twig', FALSE],
+      ['aaa-bbb.html.twig', FALSE],
+      ['aaa_bbb.html.twig', TRUE],
+      ['123.aaa-bbb.456', FALSE],
+      ['aaa_bbb', TRUE],
+      ['_aaa', TRUE],
+      ['Aaa', TRUE],
+      ['aaa-', TRUE],
+      ['aaa bbb', TRUE],
+    ];
   }
 
 }
