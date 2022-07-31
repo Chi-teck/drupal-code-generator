81e3b9fbf479a1eb721dbdff040119bc0094fe17
Chi-teck <Chi-teck@users.noreply.github.com>
2022-07-31 18:22:47 +0500

    Update service checker generator

--- src/Command/Service/AccessChecker.php
+++ src/Command/Service/AccessChecker.php
@@ -3,36 +3,36 @@
 namespace DrupalCodeGenerator\Command\Service;
 
 use DrupalCodeGenerator\Application;
-use DrupalCodeGenerator\Command\ModuleGenerator;
+use DrupalCodeGenerator\Asset\AssetCollection as Assets;
+use DrupalCodeGenerator\Attribute\Generator;
+use DrupalCodeGenerator\Command\BaseGenerator;
+use DrupalCodeGenerator\GeneratorType;
 
-/**
- * Implements service:access-checker command.
- */
-final class AccessChecker extends ModuleGenerator {
+#[Generator(
+  name: 'service:access-checker',
+  description: 'Generates an access checker service',
+  aliases: ['access-checker'],
+  templatePath: Application::TEMPLATE_PATH . '/service/access-checker',
+  type: GeneratorType::MODULE_COMPONENT,
+)]
+final class AccessChecker extends BaseGenerator {
 
-  protected string $name = 'service:access-checker';
-  protected string $description = 'Generates an access checker service';
-  protected string $alias = 'access-checker';
-  protected string $templatePath = Application::TEMPLATE_PATH . '/service/access-checker';
+  protected function generate(array &$vars, Assets $assets): void {
+    $ir = $this->createInterviewer($vars);
+    $vars['machine_name'] = $ir->askMachineName();
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function generate(array &$vars): void {
-    $this->collectDefault($vars);
-
-    $validator = static function ($value) {
-      if (!\preg_match('/^_[a-z0-9_]*[a-z0-9]$/', $value)) {
+    $validator = static function (mixed $value): string {
+      if (!\is_string($value) && !\preg_match('/^_[a-z0-9_]*[a-z0-9]$/', $value)) {
         throw new \UnexpectedValueException('The value is not correct name for "applies_to" property.');
       }
       return $value;
     };
+    $vars['applies_to'] = $ir->ask('Applies to', '_foo', $validator);
 
-    $vars['applies_to'] = $this->ask('Applies to', '_foo', $validator);
-    $vars['class'] = $this->ask('Class', '{applies_to|camelize}AccessChecker');
+    $vars['class'] = $ir->ask('Class', '{applies_to|camelize}AccessChecker');
 
-    $this->addFile('src/Access/{class}.php', 'access-checker');
-    $this->addServicesFile()->template('services');
+    $assets->addFile('src/Access/{class}.php', 'access-checker');
+    $assets->addServicesFile()->template('services');
   }
 
 }
