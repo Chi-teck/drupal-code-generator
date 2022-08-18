64fc033130e2f94d34b5e0344d3b4b5ff2008850
Chi-teck <Chi-teck@users.noreply.github.com>
2022-08-18 13:07:57 +0500

    Update menu-link generator

--- src/Command/Plugin/MenuLink.php
+++ src/Command/Plugin/MenuLink.php
@@ -3,44 +3,25 @@
 namespace DrupalCodeGenerator\Command\Plugin;
 
 use DrupalCodeGenerator\Application;
+use DrupalCodeGenerator\Asset\Assets;
+use DrupalCodeGenerator\Attribute\Generator;
+use DrupalCodeGenerator\Command\BaseGenerator;
+use DrupalCodeGenerator\GeneratorType;
 
-/**
- * Implements plugin:menu-link command.
- */
-final class MenuLink extends PluginGenerator {
+#[Generator(
+  name: 'plugin:menu-link',
+  description: 'Generates menu-link plugin',
+  aliases: ['menu-link'],
+  templatePath: Application::TEMPLATE_PATH . '/plugin/menu-link',
+  type: GeneratorType::MODULE_COMPONENT,
+)]
+final class MenuLink extends BaseGenerator {
 
-  protected string $name = 'plugin:menu-link';
-  protected string $description = 'Generates menu-link plugin';
-  protected string $alias = 'menu-link';
-  protected string $templatePath = Application::TEMPLATE_PATH . '/plugin/menu-link';
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function generate(array &$vars): void {
-    $this->collectDefault($vars);
-    $this->addFile('src/Plugin/Menu/{class}.php', 'menu-link');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function askPluginLabelQuestion(): ?string {
-    return NULL;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function askPluginIdQuestion(): ?string {
-    return NULL;
-  }
-
-  /**
-   * Asks plugin class question.
-   */
-  protected function askPluginClassQuestion(array $vars): string {
-    return $this->ask('Class', '{machine_name|camelize}MenuLink');
+  protected function generate(array &$vars, Assets $assets): void {
+    $ir = $this->createInterviewer($vars);
+    $vars['machine_name'] = $ir->askMachineName();
+    $vars['class'] = $ir->askPluginClass('Class', '{machine_name|camelize}MenuLink');
+    $assets->addFile('src/Plugin/Menu/{class}.php', 'menu-link');
   }
 
 }
