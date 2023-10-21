9a8a9cbee122c13c1c645321b3cefc9591b33af2
Chi-teck <Chi-teck@users.noreply.github.com>
2023-10-21 11:48:51 +0000

    Fix dependency injections in contatraint validator

--- tests/functional/Generator/Plugin/_constraint/_w_deps/_raw_value/src/Plugin/Validation/Constraint/AlphaConstraintValidator.php
+++ tests/functional/Generator/Plugin/_constraint/_w_deps/_raw_value/src/Plugin/Validation/Constraint/AlphaConstraintValidator.php
@@ -3,7 +3,7 @@
 namespace Drupal\foo\Plugin\Validation\Constraint;
 
 use Drupal\Core\Database\Connection;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
@@ -11,28 +11,20 @@ use Symfony\Component\Validator\ConstraintValidator;
 /**
  * Validates the Alpha constraint.
  */
-final class AlphaConstraintValidator extends ConstraintValidator implements ContainerFactoryPluginInterface {
+final class AlphaConstraintValidator extends ConstraintValidator implements ContainerInjectionInterface {
 
   /**
    * Constructs the object.
    */
   public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
     private readonly Connection $connection,
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
+  ) {}
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+  public static function create(ContainerInterface $container): self {
     return new self(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
       $container->get('database'),
     );
   }
