163b703a9d8927a7ef3bdc793969741a4996c140
Chi-teck <Chi-teck@users.noreply.github.com>
2024-04-11 17:31:23 +0000

    Update CI configuration

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,9 +8,5 @@ cd "$(dirname "$(readlink -f $0)")"/..
 echo -e "\n\e[104m Code sniffer \e[0m\n"
 ./vendor/bin/phpcs -ps
 
-# @todo  Remove this once we drop support for Drupal 10.
-drupal_version=$(composer show drupal/core | sed -n '/versions/s/^[^0-9]\+\([^,]\+\).*$/\1/p')
-if [[ $drupal_version =~ ^11 ]]; then
   echo -e "\n\e[104m Psalm \e[0m\n"
-  ./vendor/bin/psalm
-fi
+./vendor/bin/psalm
