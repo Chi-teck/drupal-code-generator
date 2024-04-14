183ae7944f4df166a60256eeb8b2d03b28b10ab2
chesn0k <spam.yureskul@gmail.com>
2024-04-14 15:29:37 +0300

    Fix HookInfoTest::testHookInfoTemplates()

--- tests/functional/Helper/Drupal/HookInfoTest.php
+++ tests/functional/Helper/Drupal/HookInfoTest.php
@@ -29,7 +29,7 @@ final class HookInfoTest extends FunctionalTestBase {
     $hook_info = new HookInfo(self::bootstrap()->get('module_handler'));
     $hook_templates = $hook_info->getHookTemplates();
     self::assertGreaterThan(260, \count($hook_templates));
-    self::assertLessThan(265, \count($hook_templates));
+    self::assertLessThan(266, \count($hook_templates));
 
     // A hook from core.api.php file.
     self::assertHookTemplate('data_type_info_alter', $hook_templates);
