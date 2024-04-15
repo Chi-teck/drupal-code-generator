1cc353ceb9f765bbd9b9e895fc9ab5bd19c7e1b9
chesn0k <spam.yureskul@gmail.com>
2024-04-15 17:35:18 +0300

    Use PHP attribute for block plugin.

--- tests/functional/Generator/Plugin/_block/_n_deps/_n_config/src/Plugin/Block/ExampleBlock.php
+++ tests/functional/Generator/Plugin/_block/_n_deps/_n_config/src/Plugin/Block/ExampleBlock.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace Drupal\foo\Plugin\Block;
 
+use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Provides an example block.
- *
- * @Block(
- *   id = "foo_example",
- *   admin_label = @Translation("Example"),
- *   category = @Translation("Custom"),
- * )
  */
+#[Block(
+  id: 'foo_example',
+  admin_label: new TranslatableMarkup('Example'),
+  category: new TranslatableMarkup('Custom'),
+)]
 final class ExampleBlock extends BlockBase {
 
   /**
