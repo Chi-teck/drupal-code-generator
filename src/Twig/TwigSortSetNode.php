277b00ab452c67eec78849af686097f9cecc4577
chesn0k <spam.yureskul@gmail.com>
2024-05-13 21:45:50 +0300

    Fix sort tag.

--- src/Twig/TwigSortSetNode.php
+++ src/Twig/TwigSortSetNode.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace DrupalCodeGenerator\Twig;
 
+use Twig\Attribute\YieldReady;
 use Twig\Compiler;
 use Twig\Node\Node;
 
 /**
  * A class that defines the compiler for 'sort' token.
  */
+#[YieldReady]
 final class TwigSortSetNode extends Node {
 
   /**
@@ -18,12 +20,13 @@ final class TwigSortSetNode extends Node {
   public function compile(Compiler $compiler): void {
     $compiler
       ->addDebugInfo($this)
-      ->write("ob_start();\n")
+      ->write('$getData = static fn () => ')
       ->subcompile($this->getNode('body'))
-      ->write('$data = explode("\n", ob_get_clean());' . "\n")
+      ->write("\$data = implode('', iterator_to_array(\$getData()));" . "\n")
+      ->write('$data = explode("\n", $data);' . "\n")
       ->write('$data = array_unique($data);' . "\n")
       ->write('sort($data, SORT_FLAG_CASE|SORT_NATURAL);' . "\n")
-      ->write('echo ltrim(implode("\n", $data)) . "\n";' . "\n");
+      ->write('yield ltrim(implode("\n", $data)) . "\n";' . "\n");
   }
 
 }
