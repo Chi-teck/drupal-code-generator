c68e0b8c1bdae726cdec9e25a8a460269045f9f6
chesn0k <spam.yureskul@gmail.com>
2024-05-21 20:06:55 +0300

    Use PHP attribute for QueueWorker plugin.

--- tests/functional/Generator/Plugin/_queue_worker/_n_deps/src/Plugin/QueueWorker/FooBar.php
+++ tests/functional/Generator/Plugin/_queue_worker/_n_deps/src/Plugin/QueueWorker/FooBar.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace Drupal\example\Plugin\QueueWorker;
 
+use Drupal\Core\Queue\Attribute\QueueWorker;
 use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Defines 'example_foo_bar' queue worker.
- *
- * @QueueWorker(
- *   id = "example_foo_bar",
- *   title = @Translation("Test"),
- *   cron = {"time" = 60},
- * )
  */
+#[QueueWorker(
+  id: 'example_foo_bar',
+  title: new TranslatableMarkup('Test'),
+  cron: ['time' => 60],
+)]
 final class FooBar extends QueueWorkerBase {
 
   /**
