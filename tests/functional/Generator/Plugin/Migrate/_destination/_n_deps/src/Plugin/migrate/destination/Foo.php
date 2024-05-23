6faa796d0d1a39c373ff0232436fab6ddc3e15c4
chesn0k <spam.yureskul@gmail.com>
2024-05-23 17:19:33 +0300

    Use PHP attribute for MigrateDestination plugin.

--- tests/functional/Generator/Plugin/Migrate/_destination/_n_deps/src/Plugin/migrate/destination/Foo.php
+++ tests/functional/Generator/Plugin/Migrate/_destination/_n_deps/src/Plugin/migrate/destination/Foo.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Drupal\example\Plugin\migrate\destination;
 
+use Drupal\migrate\Attribute\MigrateDestination;
 use Drupal\migrate\Plugin\migrate\destination\DestinationBase;
 use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Row;
 
 /**
  * The 'example_foo' destination plugin.
- *
- * @MigrateDestination(id = "example_foo")
  */
+#[MigrateDestination('example_foo')]
 final class Foo extends DestinationBase {
 
   /**
