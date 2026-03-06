2e8c0dd70d4f4d91e47f4c41d886d0c5d8962dec
Moshe Weitzman <weitzman@tejasa.com>
2026-03-06 12:20:57 -0500

    More D12 info file update

--- src/Command/BaseGenerator.php
+++ src/Command/BaseGenerator.php
@@ -288,7 +288,7 @@ abstract class BaseGenerator extends Command implements LabelInterface, IOAwareI
       type: module
       description: '@todo Add description.'
       package: '@todo Add package'
-      core_version_requirement: ^10 || ^11
+      core_version_requirement: ^10 || ^11 || ^12
 
       TWIG;
     $assets->addFile('{machine_name}.info.yml')
